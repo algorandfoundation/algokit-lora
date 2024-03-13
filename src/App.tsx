595f3949e728fc842731fee93a96e166046367a8
Patrick Dinh <patrick.dinh@makerx.com.au>
2024-03-13 15:32:46 +1000

    chore: install daisyUI

--- src/App.tsx
+++ src/App.tsx
@@ -20,7 +20,9 @@ function App() {
       </div>
       <h1>Vite + React</h1>
       <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>count is {count}</button>
+        <button className="btn btn-neutral" onClick={() => setCount((count) => count + 1)}>
+          count is {count}
+        </button>
         <p>
           Edit <code>src/App.tsx</code> and save to test HMR
         </p>
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,10 @@
+import daisyui from 'daisyui'
+
 /** @type {import('tailwindcss').Config} */
 export default {
   content: ['./index.html', './src/**/*.tsx'],
   theme: {
     extend: {},
   },
-  plugins: [],
+  plugins: [daisyui],
 }
