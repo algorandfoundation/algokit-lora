9bdac31a347bb8a82050b89ad8aa2cc7ab85cd75
Patrick Dinh <patrick.dinh@makerx.com.au>
2024-03-13 21:25:31 +1000

    chore: style the drawer

--- src/features/layout/pages/layout-page.tsx
+++ src/features/layout/pages/layout-page.tsx
@@ -8,30 +8,27 @@ export interface LayoutPageProps {
 
 export function LayoutPage({ children }: LayoutPageProps) {
   return (
-    <div>
-      <div className="drawer">
-        <input id="my-drawer" type="checkbox" className="drawer-toggle" />
-        <div className="drawer-content">
-          {/* Page content here */}
-          <label htmlFor="my-drawer" className="btn btn-primary drawer-button">
-            Open drawer
-          </label>
-        </div>
-        <div className="drawer-side">
-          <label htmlFor="my-drawer" aria-label="close sidebar" className="drawer-overlay"></label>
-          <ul className="menu bg-base-200 text-base-content min-h-full w-80 p-4">
-            <li>
-              <TemplatedNavLink urlTemplate={Urls.Feature1}>Feature 1</TemplatedNavLink>
-            </li>
-            <li>
-              <TemplatedNavLink urlTemplate={Urls.Transaction.ById} urlParams={{ transactionId: '42' }}>
-                View transaction
-              </TemplatedNavLink>
-            </li>
-          </ul>
-        </div>
+    <div className="drawer lg:drawer-open">
+      <input id="my-drawer" type="checkbox" className="drawer-toggle" />
+      <div className="drawer-content p-4">
+        {children}
+        <label htmlFor="my-drawer" className="btn btn-primary drawer-button lg:hidden">
+          Open drawer
+        </label>
       </div>
-      <div>{children}</div>
+      <div className="drawer-side">
+        <label htmlFor="my-drawer" aria-label="close sidebar" className="drawer-overlay"></label>
+        <ul className="menu bg-base-200 text-base-content min-h-full w-80 p-4">
+          <li>
+            <TemplatedNavLink urlTemplate={Urls.Feature1}>Feature 1</TemplatedNavLink>
+          </li>
+          <li>
+            <TemplatedNavLink urlTemplate={Urls.Transaction.ById} urlParams={{ transactionId: '42' }}>
+              View transaction
+            </TemplatedNavLink>
+          </li>
+        </ul>
+      </div>
     </div>
   )
 }
