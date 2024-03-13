0d8108205ccec7d16f559ec4aea62543782e06ce
Patrick Dinh <patrick.dinh@makerx.com.au>
2024-03-13 15:24:28 +1000

    chore: install jotai

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
-import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import { atom, useAtom } from 'jotai'
+
+const countAtom = atom(0)
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useAtom(countAtom)
 
   return (
     <>
