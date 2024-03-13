9f2c8a6726220d6431a08d8a284a32414fb9b495
Patrick Dinh <patrick.dinh@makerx.com.au>
2024-03-13 15:36:24 +1000

    chore: add tauri

--- src/App.tsx
+++ src/App.tsx
@@ -18,7 +18,7 @@ function App() {
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
       </div>
-      <h1>Vite + React</h1>
+      <h1>Vite + React on Tauri</h1>
       <div className="card">
         <button className="btn btn-neutral" onClick={() => setCount((count) => count + 1)}>
           count is {count}
--- vite.config.ts
+++ vite.config.ts
@@ -8,4 +8,18 @@ export default defineConfig({
   test: {
     root: 'tests',
   },
+
+  // Vite options tailored for Tauri development and only applied in `tauri dev` or `tauri build`
+  //
+  // 1. prevent vite from obscuring rust errors
+  clearScreen: false,
+  // 2. tauri expects a fixed port, fail if that port is not available
+  server: {
+    port: 1420,
+    strictPort: true,
+    watch: {
+      // 3. tell vite to ignore watching `src-tauri`
+      ignored: ['**/src-tauri/**'],
+    },
+  },
 })
