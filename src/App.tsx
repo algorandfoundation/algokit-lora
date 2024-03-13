e334f926d8412a21d91e67e678a67a3470a1b1f2
Patrick Dinh <patrick.dinh@makerx.com.au>
2024-03-13 15:21:07 +1000

    chore: config for tailwind

--- src/App.tsx
+++ src/App.tsx
@@ -23,7 +23,7 @@ function App() {
           Edit <code>src/App.tsx</code> and save to test HMR
         </p>
       </div>
-      <h1 className="text-3xl font-bold underline">Hello world!</h1>
+      <h1 className="bg-sky-700 text-3xl font-bold underline">Hello world!</h1>
       <p className="read-the-docs">Click on the Vite and React logos to learn more</p>
     </>
   )
