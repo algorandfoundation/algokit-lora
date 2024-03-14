18fe34c90ec068bc742d92875a3d0d855fdb04a3
Patrick Dinh <patrick.dinh@makerx.com.au>
2024-03-15 09:50:47 +1000

    chore: fix colors

--- src/features/theme/components/theme-toggle.tsx
+++ src/features/theme/components/theme-toggle.tsx
@@ -3,6 +3,7 @@ import { Moon, Sun } from 'lucide-react'
 import { Button } from '@/features/primitive/components/button'
 import { DropdownMenu, DropdownMenuContent, DropdownMenuItem, DropdownMenuTrigger } from '@/features/primitive/components/dropdown-menu'
 import { useTheme } from '../hooks/use-theme'
+import { cn } from '@/features/primitive/utils'
 
 export function ThemeToggle() {
   const { setTheme } = useTheme()
@@ -10,7 +11,7 @@ export function ThemeToggle() {
   return (
     <DropdownMenu>
       <DropdownMenuTrigger asChild>
-        <Button variant="outline" size="icon">
+        <Button variant="outline" size="icon" className={cn('bg-transparent')}>
           <Sun className="h-[1.2rem] w-[1.2rem] rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0" />
           <Moon className="absolute h-[1.2rem] w-[1.2rem] rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100" />
           <span className="sr-only">Toggle theme</span>
