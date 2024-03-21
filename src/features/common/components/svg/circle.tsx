10ff1c5968e5854ceb2ce68f369d0e9b8eae934a
Patrick Dinh <patrick.dinh@makerx.com.au>
2024-03-22 08:57:42 +1000

    chore: remove React import from svg code gen file

--- src/features/common/components/svg/circle.tsx
+++ src/features/common/components/svg/circle.tsx
@@ -1,4 +1,3 @@
-import * as React from "react";
 import type { SVGProps } from "react";
 const SvgCircle = (props: SVGProps<SVGSVGElement>) => <svg xmlnsXlink="http://www.w3.org/1999/xlink" width="21px" height="21px" viewBox="0 0 21 21" xmlns="http://www.w3.org/2000/svg" {...props}><g transform="matrix(1 0 0 1 -153 -143 )"><path d="M 163.5 143  C 169.38 143  174 147.62  174 153.5  C 174 159.38  169.38 164  163.5 164  C 157.62 164  153 159.38  153 153.5  C 153 147.62  157.62 143  163.5 143  Z " fillRule="nonzero" fill="currentColor" stroke="none" /></g></svg>;
 export default SvgCircle;
