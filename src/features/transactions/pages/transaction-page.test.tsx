5579070e444dfa937c2541d623191d6c203b24a6
Hoang Dinh <patrick.dinh@makerx.com.au>
2024-04-05 16:43:45 +1000

    chore: test

--- src/features/transactions/pages/transaction-page.test.tsx
+++ src/features/transactions/pages/transaction-page.test.tsx
@@ -2,16 +2,19 @@ import { transactionModelMother } from '@/tests/object-mother/transaction-model'
 import { describe, expect, it, vi } from 'vitest'
 import { TransactionPage, getSampleTransaction } from './transaction-page'
 import { executeComponentTest } from '@/tests/test-component'
-import { render, waitFor, within } from '@/tests/testing-library'
+import { render, within } from '@/tests/testing-library'
+import { useParams } from 'react-router-dom'
+
+vi.mock('react-router-dom', () => ({
+  ...vi.importActual('react-router-dom'),
+  useParams: vi.fn(),
+}))
 
 describe('given a payment transaction', () => {
   const paymentTransaction = transactionModelMother.paymentTransaction().build()
 
   it('it should be rendered', async () => {
-    vi.mock('react-router-dom', () => ({
-      ...vi.importActual('react-router-dom'),
-      useParams: () => ({ transactionId: paymentTransaction.id }),
-    }))
+    vi.mocked(useParams).mockImplementation(() => ({ transactionId: paymentTransaction.id }))
 
     const mock = vi.fn().mockImplementation(getSampleTransaction)
     mock.mockImplementationOnce(() => paymentTransaction)
