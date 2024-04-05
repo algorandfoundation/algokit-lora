33f802df9e5053414c7c7afab470d2be542abdc3
Negar Abbasi <negar.abbasi@makerx.com.au>
2024-04-05 15:09:21 +1000

    test: add unit tests

--- src/features/transactions/components/transaction-not-existing.tsx
+++
@@ -1,9 +0,0 @@
-import { cn } from '@/features/common/utils'
-
-export function TransactionNotExist() {
-  return (
-    <div>
-      <h1 className={cn('text-2xl text-primary font-bold')}>Transaction does not exist</h1>
-    </div>
-  )
-}
