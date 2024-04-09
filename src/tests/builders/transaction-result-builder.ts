e967550dbb5a837bac723a438fabcc107788c564
Patrick Dinh <patrick.dinh@makerx.com.au>
2024-04-09 22:10:55 +1000

    chore: clean up

--- src/tests/builders/transaction-result-builder.ts
+++ src/tests/builders/transaction-result-builder.ts
@@ -1,4 +1,4 @@
-import { DataBuilder, dossierProxy, randomNumber, randomString } from '@makerx/ts-dossier'
+import { DataBuilder, dossierProxy, randomElement, randomNumber, randomString } from '@makerx/ts-dossier'
 import { TransactionResult } from '@algorandfoundation/algokit-utils/types/indexer'
 import algosdk from 'algosdk'
 
@@ -6,17 +6,11 @@ export class TransactionResultBuilder extends DataBuilder<TransactionResult> {
   constructor() {
     super({
       id: randomString(52, 52),
-      'tx-type': algosdk.TransactionType.pay,
+      'tx-type': randomElement(Object.values(algosdk.TransactionType)),
       'last-valid': randomNumber(),
       'first-valid': randomNumber(),
       fee: randomNumber(),
       sender: randomString(52, 52),
-      'confirmed-round': randomNumber(),
-      'round-time': randomNumber(),
-      'payment-transaction': {
-        amount: randomNumber(),
-        receiver: randomString(52, 52),
-      },
     })
   }
 }
