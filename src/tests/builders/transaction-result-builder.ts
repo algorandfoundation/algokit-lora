d3e2ba717506059f68c5f33bc367b6c9d9d17cd8
Patrick Dinh <patrick.dinh@makerx.com.au>
2024-04-09 21:32:17 +1000

    chore: clean up

--- src/tests/builders/transaction-result-builder.ts
+++ src/tests/builders/transaction-result-builder.ts
@@ -4,18 +4,6 @@ import algosdk from 'algosdk'
 
 export class TransactionResultBuilder extends DataBuilder<TransactionResult> {
   constructor() {
-    // const base: Omit<PaymentTransactionModel, 'json'> = {
-    //   id: randomString(52, 52),
-    //   sender: randomString(52, 52),
-    //   type: TransactionType.Payment,
-    //   confirmedRound: randomNumber(),
-    //   roundTime: randomNumber(),
-    //   receiver: randomString(52, 52),
-    //   amount: new AlgoAmount({ microAlgos: randomNumber() }),
-    //   closeAmount: new AlgoAmount({ microAlgos: randomNumber() }),
-    //   fee: new AlgoAmount({ microAlgos: randomNumber() }),
-    // }
-
     super({
       id: randomString(52, 52),
       'tx-type': algosdk.TransactionType.pay,
