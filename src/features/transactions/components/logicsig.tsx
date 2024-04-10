187c771bdf260e7330adc7e975f2c49f6b12b8ae
Negar Abbasi <negar.abbasi@makerx.com.au>
2024-04-10 15:14:03 +1000

    fix: representation of logicsig

--- src/features/transactions/components/logicsig.tsx
+++ src/features/transactions/components/logicsig.tsx
@@ -1,32 +1,38 @@
 import { LogicsigModel } from '../models'
 import { Card, CardContent } from '@/features/common/components/card'
 import { cn } from '@/features/common/utils'
-import { useMemo } from 'react'
-import { DescriptionList } from '@/features/common/components/description-list'
-import { transactionPageConstants } from '@/features/theme/constant'
+import { Tabs, TabsContent, TabsList, TabsTrigger } from '@radix-ui/react-tabs'
 
 export type LogicsigProps = {
   logicsig: LogicsigModel
 }
 
-export function Logicsig({ logicsig: logicsig }: LogicsigProps) {
-  const logicsigItems = useMemo(
-    () => [
-      {
-        dt: transactionPageConstants.labels.signature.logicsig,
-        dd: logicsig.logic,
-      },
-    ],
-    [logicsig.logic]
-  )
+export function Logicsig({ logicsig }: LogicsigProps) {
   return (
-    <Card className={cn('p-4')}>
-      <CardContent className={cn('text-sm space-y-4')}>
-        <div className={cn('space-y-2')}>
-          <h1 className={cn('text-2xl text-primary font-bold')}>Logicsig</h1>
-          <DescriptionList items={logicsigItems} />
-        </div>
-      </CardContent>
-    </Card>
+    <>
+      <h1 className={cn('text-2xl text-primary font-bold')}>Logicsig</h1>
+      <Tabs defaultValue="Logicsig">
+        <TabsList aria-label="View Transaction">
+          <TabsTrigger className={cn('data-[state=active]:border-primary data-[state=active]:border-b-2 w-32')} value="Base64">
+            Base64
+          </TabsTrigger>
+          <TabsTrigger className={cn('data-[state=active]:border-primary data-[state=active]:border-b-2 w-32')} value="Teal">
+            Teal
+          </TabsTrigger>
+        </TabsList>
+        <TabsContent value="Base64" className={cn('border-solid border-2 border-border p-4')}>
+          <Card className={cn('p-4')}>
+            <CardContent className={cn('text-sm space-y-4')}>
+              <div className={cn('space-y-2')}>
+                <p>{logicsig.logic}</p>
+              </div>
+            </CardContent>
+          </Card>
+        </TabsContent>
+        <TabsContent value="Teal" className={cn('border-solid border-2 border-border p-4')}>
+          <h1>Teal</h1>
+        </TabsContent>
+      </Tabs>
+    </>
   )
 }
