0f8af8e410b68bd253a1bb7845c60b0ee0822e03
Patrick Dinh <patrick.dinh@makerx.com.au>
2024-04-11 15:21:36 +1000

    chore: delete code

--- src/features/transactions/components/transaction.tsx
+++ src/features/transactions/components/transaction.tsx
@@ -1,58 +1,10 @@
 import { PaymentTransaction } from './payment-transaction'
-import { TransactionResult, TransactionSignature } from '@algorandfoundation/algokit-utils/types/indexer'
-import { LogicsigModel, MultisigModel, PaymentTransactionModel, SignatureType, SinglesigModel, TransactionType } from '../models'
+import { TransactionResult } from '@algorandfoundation/algokit-utils/types/indexer'
 import algosdk from 'algosdk'
-import { invariant } from '@/utils/invariant'
-import { publicKeyToAddress } from '@/utils/publickey-to-addess'
+import { asPaymentTransaction } from '../mappers/transaction-mappers'
 
 type Props = {
   transaction: TransactionResult
-}
-
-const asPaymentTransaction = (transaction: TransactionResult): PaymentTransactionModel => {
-  invariant(transaction['confirmed-round'], 'confirmed-round is not set')
-  invariant(transaction['round-time'], 'round-time is not set')
-  invariant(transaction['payment-transaction'], 'payment-transaction is not set')
-
-  // TODO: Handle notes
-  return {
-    id: transaction.id,
-    type: TransactionType.Payment,
-    confirmedRound: transaction['confirmed-round'],
-    roundTime: transaction['round-time'] * 1000,
-    group: transaction['group'],
-    fee: transaction.fee.microAlgos(),
-    sender: transaction.sender,
-    receiver: transaction['payment-transaction']['receiver'],
-    amount: transaction['payment-transaction']['amount'].microAlgos(),
-    closeAmount: transaction['payment-transaction']['close-amount']?.microAlgos(),
-    signature: transformSignature(transaction.signature),
-  } satisfies PaymentTransactionModel
-}
-
-const transformSignature = (signature?: TransactionSignature) => {
-  if (signature?.sig) {
-    return {
-      type: SignatureType.Single,
-      signer: signature.sig,
-    } satisfies SinglesigModel
-  }
-
-  if (signature?.multisig) {
-    return {
-      type: SignatureType.Multi,
-      version: signature.multisig.version,
-      threshold: signature.multisig.threshold,
-      subsigners: signature.multisig.subsignature.map((subsignature) => publicKeyToAddress(subsignature['public-key'])),
-    } satisfies MultisigModel
-  }
-
-  if (signature?.logicsig) {
-    return {
-      type: SignatureType.Logic,
-      logic: signature.logicsig.logic,
-    } satisfies LogicsigModel
-  }
 }
 
 export function Transaction({ transaction }: Props) {
