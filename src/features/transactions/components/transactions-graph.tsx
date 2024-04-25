f950d21645892e2b7c277de7a8c37160289fface
Patrick Dinh <patrick.dinh@makerx.com.au>
2024-04-25 15:37:11 +1000

    chore: clean up

--- src/features/transactions/components/transactions-graph.tsx
+++ src/features/transactions/components/transactions-graph.tsx
@@ -30,7 +30,6 @@ import { InnerTransactionLink } from './inner-transaction-link'
 import { assetIdLabel } from './asset-config-transaction-info'
 import { assetFreezeAddressLabel } from './asset-freeze-transaction-info'
 import { TransactionLink } from './transaction-link'
-import { Collaborator, getTransactionsCollaborators } from '../utils/get-transactions-collaborators'
 import { graphConfig } from '../utils/graph-config'
 import { ellipseAddress } from '@/utils/ellipse-address'
 import { flattenInnerTransactions } from '@/utils/flatten-inner-transactions'
@@ -582,4 +581,55 @@ export function TransactionsGraph({ transactions }: Props) {
       ))}
     </div>
   )
+}
+
+const getTransactionsCollaborators = (transactions: Transaction[]): Collaborator[] => {
+  return transactions.reduce((acc, transaction) => {
+    const collaborators = getTransactionCollaborators(transaction)
+    collaborators.forEach((collaborator) => {
+      if (!acc.some((c) => c.type === collaborator.type && c.id === collaborator.id)) {
+        acc.push(collaborator)
+      }
+    })
+    return acc
+  }, new Array<Collaborator>())
+}
+
+const getTransactionCollaborators = (transaction: Transaction | InnerTransaction): Collaborator[] => {
+  const collaborators: Collaborator[] = [
+    {
+      type: 'Account',
+      id: transaction.sender,
+    },
+  ]
+  if (transaction.type === TransactionType.Payment || transaction.type === TransactionType.AssetTransfer) {
+    collaborators.push({
+      type: 'Account',
+      id: transaction.receiver,
+    })
+  }
+  if (transaction.type === TransactionType.ApplicationCall) {
+    collaborators.push({
+      type: 'Application',
+      id: transaction.applicationId.toString(),
+    })
+  }
+  if (transaction.type === TransactionType.AssetConfig) {
+    collaborators.push({
+      type: 'Asset',
+      id: transaction.assetId.toString(),
+    })
+  }
+  if (transaction.type === TransactionType.AssetFreeze) {
+    collaborators.push({
+      type: 'Asset',
+      id: transaction.assetId.toString(),
+    })
+  }
+  return collaborators
+}
+
+type Collaborator = {
+  type: 'Account' | 'Application' | 'Asset'
+  id: string
 }
