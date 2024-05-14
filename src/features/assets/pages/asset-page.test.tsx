2c529ef12f1b9c76c2e3be847b7b988143f53d40
Patrick Dinh <patrick.dinh@makerx.com.au>
2024-05-14 21:31:45 +1000

    add tests

--- src/features/assets/pages/asset-page.test.tsx
+++ src/features/assets/pages/asset-page.test.tsx
@@ -17,6 +17,7 @@ import {
   assetReserveLabel,
   assetTotalSupplyLabel,
   assetTraitsLabel,
+  assetTransactionsLabel,
   assetUrlLabel,
 } from '../components/labels'
 import { useParams } from 'react-router-dom'
@@ -146,6 +147,10 @@ describe('asset-page', () => {
                 { term: 'Image Mimetype', description: 'image/png' },
               ],
             })
+
+            const transactionTabList = component.getByRole('tablist', { name: assetTransactionsLabel })
+            expect(transactionTabList).toBeTruthy()
+            expect(transactionTabList.children.length).toBe(2)
           })
         }
       )
@@ -607,6 +612,9 @@ describe('asset-page', () => {
 
             const assetTraitsCard = component.queryByText(assetTraitsLabel)
             expect(assetTraitsCard).toBeNull()
+
+            const transactionTabList = component.queryByRole('tablist', { name: assetTransactionsLabel })
+            expect(transactionTabList).toBeNull()
           })
         }
       )
