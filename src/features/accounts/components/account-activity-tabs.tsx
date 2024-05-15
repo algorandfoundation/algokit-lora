95f1065d451bbee3a37845795076ba20912530af
Negar Abbasi <negar.abbasi@makerx.com.au>
2024-05-15 14:43:15 +1000

    refactor: tabs PR review

--- src/features/accounts/components/account-activity-tabs.tsx
+++ src/features/accounts/components/account-activity-tabs.tsx
@@ -1,5 +1,6 @@
-import { Tabs, TabsList, TabsTrigger } from '@/features/common/components/tabs'
+import { OverflowAutoTabsContent, Tabs, TabsList, TabsTrigger } from '@/features/common/components/tabs'
 import { cn } from '@/features/common/utils'
+import { useMemo } from 'react'
 
 const accountVisualTransactionTabId = 'visual'
 const accountVisualAssetsTabId = 'table'
@@ -14,40 +15,52 @@ export const accountVisualCreatedApplicationsTabLabel = 'Created Applications'
 export const accountVisualOptedApplicationsTabLabel = 'Opted Applications'
 
 export function AccountActivityTabs() {
+  const tabs = useMemo(
+    () => [
+      {
+        id: accountVisualTransactionTabId,
+        label: accountDetailsLabel,
+        children: '',
+      },
+      {
+        id: accountVisualAssetsTabId,
+        label: accountVisualAssetsTabLabel,
+        children: '',
+      },
+      {
+        id: accountVisualCreatedAssetsTabId,
+        label: accountVisualCreatedAssetsTabLabel,
+        children: '',
+      },
+      {
+        id: accountVisualCreatedApplicationsTabId,
+        label: accountVisualCreatedApplicationsTabLabel,
+        children: '',
+      },
+      {
+        id: accountVisualOptedApplicationsTabId,
+        label: accountVisualOptedApplicationsTabLabel,
+        children: '',
+      },
+    ],
+    []
+  )
   return (
     <Tabs defaultValue={accountVisualTransactionTabId}>
       <TabsList aria-label={accountDetailsLabel}>
-        <TabsTrigger
-          className={cn('data-[state=active]:border-primary data-[state=active]:border-b-2 w-44')}
-          value={accountVisualTransactionTabId}
-        >
-          {accountVisualGraphTabLabel}
-        </TabsTrigger>
-        <TabsTrigger
-          className={cn('data-[state=active]:border-primary data-[state=active]:border-b-2 w-44')}
-          value={accountVisualAssetsTabId}
-        >
-          {accountVisualAssetsTabLabel}
-        </TabsTrigger>
-        <TabsTrigger
-          className={cn('data-[state=active]:border-primary data-[state=active]:border-b-2 w-44')}
-          value={accountVisualCreatedAssetsTabId}
-        >
-          {accountVisualCreatedAssetsTabLabel}
-        </TabsTrigger>
-        <TabsTrigger
-          className={cn('data-[state=active]:border-primary data-[state=active]:border-b-2 w-44')}
-          value={accountVisualCreatedApplicationsTabId}
-        >
-          {accountVisualCreatedApplicationsTabLabel}
-        </TabsTrigger>
-        <TabsTrigger
-          className={cn('data-[state=active]:border-primary data-[state=active]:border-b-2 w-44')}
-          value={accountVisualOptedApplicationsTabId}
-        >
-          {accountVisualOptedApplicationsTabLabel}
-        </TabsTrigger>
+        {tabs.map((tab) => (
+          <TabsTrigger key={tab.id} className={cn('data-[state=active]:border-primary data-[state=active]:border-b-2 w-44')} value={tab.id}>
+            {tab.label}
+          </TabsTrigger>
+        ))}
       </TabsList>
+      {tabs.map((tab) => (
+        <OverflowAutoTabsContent key={tab.id} value={tab.id} className={cn('border-solid border-2 border-border')}>
+          <div className="grid">
+            <div className="overflow-auto p-4">{tab.children}</div>
+          </div>
+        </OverflowAutoTabsContent>
+      ))}
     </Tabs>
   )
 }
