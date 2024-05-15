d4ca195bdb21d989a965b4ece6fb0821b08a0ee2
Patrick Dinh <patrick.dinh@makerx.com.au>
2024-05-15 11:09:06 +1000

    update the comment

--- src/features/assets/data/asset-metadata.ts
+++ src/features/assets/data/asset-metadata.ts
@@ -90,8 +90,7 @@ export const fetchAssetMetadataAtomBuilder = (assetResult: AssetResult) =>
             .then((res) => res.transactions as TransactionResult[]) // Implicitly newest to oldest when filtering with an address
         : []
     if (results.length === 0) {
-      // The asset has been destroyed or is an immutable asset.
-      // Or the asset doesn't have any config transactions by the manager account.
+      // The asset has been destroyed or is an immutable asset, or the asset is mutable however has not been mutated.
       // Fetch the entire acfg transaction history and reverse the order, so it's newest to oldest
       results = await executePaginatedRequest(
         (res: TransactionSearchResults) => res.transactions,
