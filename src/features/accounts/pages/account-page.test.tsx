29e15cb21099eef1bceaa527fbe59ae1c84eaa14
Negar Abbasi <negar.abbasi@makerx.com.au>
2024-05-16 17:52:24 +1000

    test: remove a redundant test

--- src/features/accounts/pages/account-page.test.tsx
+++ src/features/accounts/pages/account-page.test.tsx
@@ -52,38 +52,6 @@ describe('account-page', () => {
   })
 
   describe('when rendering an account', () => {
-    const accountResult = accountResultMother['mainnet-7AHHR4ZMHKMRFUVGLU3SWGKMJBKRUA5UQQUPFWT4WMFO2RLXBUIXZR7FQQ']().build()
-
-    it('should be rendered with the correct data', () => {
-      const myStore = createStore()
-      myStore.set(accountResultsAtom, new Map([[accountResult.address, atom(accountResult)]]))
-
-      vi.mocked(useParams).mockImplementation(() => ({ address: accountResult.address }))
-
-      return executeComponentTest(
-        () => render(<AccountPage />, undefined, myStore),
-        async (component) => {
-          await waitFor(() => {
-            const informationCard = component.getByLabelText(accountInformationLabel)
-            descriptionListAssertion({
-              container: informationCard,
-              items: [
-                { term: accountAddressLabel, description: '7AHHR4ZMHKMRFUVGLU3SWGKMJBKRUA5UQQUPFWT4WMFO2RLXBUIXZR7FQQ' },
-                { term: accountBalanceLabel, description: '5.85446' },
-                { term: accountMinBalanceLabel, description: '0.2' },
-                { term: accountAssetsHeldLabel, description: '1' },
-                { term: accountAssetsCreatedLabel, description: '0' },
-                { term: accountAssetsOptedInLabel, description: '1' },
-                { term: accountApplicationsCreatedLabel, description: '0' },
-                { term: accountApplicationsOptedInLabel, description: '0' },
-              ],
-            })
-          })
-        }
-      )
-    })
-  })
-  describe('when rendering an new account', () => {
     const accountResult = accountResultMother['mainnet-BIQXAK67KSCKN3EJXT4S3RVXUBFOLZ45IQOBTSOQWOSR4LLULBTD54S5IA']().build()
 
     it('should be rendered with the correct data', () => {
--- src/tests/object-mother/account-result.ts
+++ src/tests/object-mother/account-result.ts
@@ -3,29 +3,6 @@ import { AccountResultBuilder } from '../builders/account-result-builder'
 import { AccountStatus } from '@algorandfoundation/algokit-utils/types/indexer'
 
 export const accountResultMother = {
-  ['mainnet-7AHHR4ZMHKMRFUVGLU3SWGKMJBKRUA5UQQUPFWT4WMFO2RLXBUIXZR7FQQ']: () => {
-    return new AccountResultBuilder({
-      address: '7AHHR4ZMHKMRFUVGLU3SWGKMJBKRUA5UQQUPFWT4WMFO2RLXBUIXZR7FQQ',
-      amount: 5854460,
-      'amount-without-pending-rewards': 5854460,
-      'apps-local-state': [],
-      'apps-total-schema': { 'num-byte-slice': 0, 'num-uint': 0 },
-      assets: [{ amount: 5177280000, 'asset-id': 924268058, 'is-frozen': false }],
-      'created-apps': [],
-      'created-assets': [],
-      'min-balance': 200000,
-      'pending-rewards': 0,
-      'reward-base': 218288,
-      rewards: 0,
-      round: 38851889,
-      status: AccountStatus.Offline,
-      'total-apps-opted-in': 0,
-      'total-assets-opted-in': 1,
-      'total-created-apps': 0,
-      'total-created-assets': 0,
-    } satisfies AccountResult)
-  },
-
   ['mainnet-BIQXAK67KSCKN3EJXT4S3RVXUBFOLZ45IQOBTSOQWOSR4LLULBTD54S5IA']: () => {
     return new AccountResultBuilder({
       address: 'BIQXAK67KSCKN3EJXT4S3RVXUBFOLZ45IQOBTSOQWOSR4LLULBTD54S5IA',
