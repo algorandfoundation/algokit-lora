cd5063d8b148ce4affbdea4ee40b67dbff7d32df
Patrick Dinh <patrick.dinh@makerx.com.au>
2024-05-23 16:14:23 +1000

    Init indexer and algod clients when the page is loaded

--- src/features/common/data/index.ts
+++ src/features/common/data/index.ts
@@ -1,14 +1,17 @@
 import { Config, getAlgoClient, getAlgoIndexerClient } from '@algorandfoundation/algokit-utils'
-import { NetworkConfig, localnetConfig } from '../../settings/data/network'
+import { NetworkConfig, networkConfigAtom } from '../../settings/data/network'
+import { settingsStore } from '@/features/settings/data'
 export * from './atom-with-debounce'
 
 Config.configure({
   logger: Config.getLogger(true),
 })
 
-export let indexer = getAlgoIndexerClient(localnetConfig.indexer)
+// Init the network config from local storage
+const networkConfig = settingsStore.get(networkConfigAtom)
+export let indexer = getAlgoIndexerClient(networkConfig.indexer)
 
-export let algod = getAlgoClient(localnetConfig.algod)
+export let algod = getAlgoClient(networkConfig.algod)
 
 export const setNetwork = (networkConfig: NetworkConfig) => {
   indexer = getAlgoIndexerClient(networkConfig.indexer)
