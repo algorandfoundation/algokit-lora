d37199ca49514c08cf57ed0d44e48ae2573a8335
Hoang Dinh <patrick.dinh@makerx.com.au>
2024-05-26 21:21:28 +1000

    chore: add comments on why we implement the lazy load pagination we way we did (#67)

--- src/features/common/constants.ts
+++ src/features/common/constants.ts
@@ -1,5 +1,9 @@
 export const ZERO_ADDRESS = 'AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAY5HFKQ'
 // The default limit when we fetch data from the indexer
-// In most places, we use page size 10, 20, 30, 40, 50.
-// 100 items will give us at least 2 pages of data.
-export const DEFAULT_FETCH_SIZE = 100
+// We chose 100 because:
+//   - In most places, we use page size 10, 20, 30, 40, 50. 100 items will give us at least 2 pages of data.
+//   - 100 is small enough to not cause performance issues.
+//     For example, for the account EXECUTOREZZDJN3NFLVZNXE7UZKCERNFC6FD7SBXMVKASKRF4ZASJKN5BE
+//     the response size of fetching 100 items is 180Kb (Gzipped), which is ok.
+//     if we up it to 200, the response is 400Kb and can cause performance issues.
+export const DEFAULT_FETCH_SIZE = 200
