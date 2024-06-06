897d39a3ad4f1130dbce0ec799b3911c3cfdf1d6
Patrick Dinh <patrick.dinh@makerx.com.au>
2024-06-06 11:49:54 +1000

    Hack the left side bar

--- src/features/layout/components/left-side-bar-menu.tsx
+++ src/features/layout/components/left-side-bar-menu.tsx
@@ -6,10 +6,11 @@ import SvgCodeBlock from '@/features/common/components/icons/code-block'
 import SvgHome from '@/features/common/components/icons/home'
 import { Button } from '@/features/common/components/button'
 import SvgChevronLeft from '@/features/common/components/icons/chevron-left'
-import { useCallback } from 'react'
+import { useCallback, useMemo } from 'react'
 import SvgChevronRight from '@/features/common/components/icons/chevron-right'
 import SvgCog from '@/features/common/components/icons/cog'
 import { useLayout } from '@/features/settings/data'
+import { useLocation } from 'react-router-dom'
 
 type Props = {
   className?: string
@@ -28,6 +29,21 @@ export function LeftSideBarMenu({ className }: Props) {
     [setLayout]
   )
 
+  // The little hack to make the index menu item active when transaction, block, account, asset, application are viewed
+  // This needs to be done because by default React router doesn't match the index URLs with any sub-path
+  // The doc: https://reactrouter.com/en/main/components/nav-link#end
+  const location = useLocation()
+  const isIndexActive = useMemo(() => {
+    const forceMatchWithIndex = [
+      Urls.Transaction.build({}),
+      Urls.Block.build({}),
+      Urls.Account.build({}),
+      Urls.Asset.build({}),
+      Urls.Application.build({}),
+    ]
+    return forceMatchWithIndex.some((path) => location.pathname.startsWith(path))
+  }, [location.pathname])
+
   return (
     <NavigationMenu
       className={cn('bg-card transition-all duration-300 min-h-screen', className, layout.isLeftSideBarExpanded ? 'w-52' : 'w-10')}
@@ -43,7 +59,10 @@ export function LeftSideBarMenu({ className }: Props) {
             <NavigationMenuLink asChild>
               <TemplatedNavLink
                 urlTemplate={menuItem.urlTemplate}
-                className={cn('[&.active]:text-primary flex items-center p-2 gap-2 min-h-10 pl-3 whitespace-nowrap')}
+                className={cn(
+                  '[&.active]:text-primary flex items-center p-2 gap-2 min-h-10 pl-3 whitespace-nowrap',
+                  menuItem.urlTemplate === Urls.Index && isIndexActive ? 'active' : ''
+                )}
               >
                 <div className={cn('text-primary')}>{menuItem.icon}</div>
                 <div className={cn(layout.isLeftSideBarExpanded ? 'visible delay-100' : 'invisible delay-100')}>{menuItem.text}</div>
