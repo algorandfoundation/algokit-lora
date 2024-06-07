8eb334741965bdc31de0a72737e84b37b5179bda
Negar Abbasi <negar.abbasi@makerx.com.au>
2024-06-07 15:18:37 +1000

    PR review

--- src/features/wallet/components/connect-wallet-button.tsx
+++ src/features/wallet/components/connect-wallet-button.tsx
@@ -6,7 +6,7 @@ import { ellipseAddress } from '@/utils/ellipse-address'
 import { buttonVariants } from '@/features/common/components/button'
 import { AccountLink } from '@/features/accounts/components/account-link'
 import { Loader2 as Loader, CircleMinus, Wallet } from 'lucide-react'
-import { useNetworkConfig } from '@/features/settings/data'
+import { localnetConfig, useNetworkConfig } from '@/features/settings/data'
 import { useCallback, useMemo, useState } from 'react'
 import { Popover, PopoverContent, PopoverTrigger } from '@/features/common/components/popover'
 import { Select, SelectContent, SelectItem, SelectTrigger, SelectValue } from '@/features/common/components/select'
@@ -55,14 +55,14 @@ export function ConnectWallet({ activeAddress, providers }: ConnectWalletProps) 
             {!activeAddress &&
               availableProviders.map((provider) => (
                 <Button key={`provider-${provider.metadata.id}`} onClick={selectProvider(provider)}>
-                  {provider.metadata.id !== 'mnemonic' && provider.metadata.id !== 'kmd' ? (
+                  {localnetConfig.walletProviders.includes(provider.metadata.id) ? (
+                    <Wallet className={cn('size-6 rounded object-contain mr-2')} />
+                  ) : (
                     <img
                       src={provider.metadata.icon}
                       alt={`${provider.metadata.name} icon`}
                       className="h-auto w-6 rounded object-contain"
                     />
-                  ) : (
-                    <Wallet className={cn('size-6 rounded object-contain mr-2')} />
                   )}
                   <span className="ml-1">{provider.metadata.name}</span>
                 </Button>
@@ -116,15 +116,16 @@ export function ConnectedWallet({ activeAddress, connectedActiveAccounts, provid
     <Popover>
       <PopoverTrigger asChild>
         <Button className="w-36" variant="default">
-          {activeProvider && activeProvider.metadata.id !== 'mnemonic' && activeProvider.metadata.id !== 'kmd' ? (
-            <img
-              src={activeProvider.metadata.icon}
-              alt={`${activeProvider.metadata.name} icon`}
-              className={cn('size-6 rounded object-contain mr-2')}
-            />
-          ) : (
-            <Wallet className={cn('size-6 rounded object-contain mr-2')} />
-          )}
+          {activeProvider &&
+            (localnetConfig.walletProviders.includes(activeProvider.metadata.id) ? (
+              <Wallet className={cn('size-6 rounded object-contain mr-2')} />
+            ) : (
+              <img
+                src={activeProvider.metadata.icon}
+                alt={`${activeProvider.metadata.name} icon`}
+                className={cn('size-6 rounded object-contain mr-2')}
+              />
+            ))}
           <abbr title={activeAddress} className="no-underline">
             {ellipseAddress(activeAddress)}
           </abbr>
@@ -139,7 +140,7 @@ export function ConnectedWallet({ activeAddress, connectedActiveAccounts, provid
               <Label hidden={true} htmlFor="account">
                 Select Account
               </Label>
-              <Select onValueChange={switchAccount} value={activeAddress}>
+              <Select onValueChange={switchAccount} value={activeAddress} aria-label="accountSwitcher">
                 <SelectTrigger id="account" className={cn('h-9')}>
                   <SelectValue placeholder="Select account" />
                 </SelectTrigger>
