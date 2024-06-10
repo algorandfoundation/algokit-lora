c32036a2e6cc8f69b2fe407951979a395daf6731
Neil Campbell <neil.campbell@makerx.com.au>
2024-06-10 15:13:54 +0800

    fix: expire old latest transaction ids (#89)

--- src/features/common/data/state-cleanup.ts
+++ src/features/common/data/state-cleanup.ts
@@ -1,6 +1,6 @@
 import { blockResultsAtom } from '@/features/blocks/data'
 import { groupResultsAtom } from '@/features/groups/data'
-import { transactionResultsAtom } from '@/features/transactions/data'
+import { latestTransactionIdsAtom, transactionResultsAtom } from '@/features/transactions/data'
 import { Atom, Getter, PrimitiveAtom, Setter, useAtom } from 'jotai'
 import { atomEffect } from 'jotai-effect'
 import { accountResultsAtom } from '@/features/accounts/data'
@@ -8,24 +8,30 @@ import { applicationMetadataResultsAtom } from '@/features/applications/data/app
 import { applicationResultsAtom } from '@/features/applications/data'
 import { assetMetadataResultsAtom, assetResultsAtom } from '@/features/assets/data'
 
-const cleanUpIntervalMillis = 60_000 // 10 minutes
-const expirationMillis = 3600000 // 1 hour
+const cleanUpIntervalMillis = 600_000 // 10 minutes
+const expirationMillis = 3_600_000 // 1 hour
 // Run every 10 minutes and cleanup data that hasn't been accessed in the last 1 hour
 
 const stateCleanupEffect = atomEffect((get, set) => {
   const cleanup = setInterval(() => {
-    ;(async () => {
-      const expiredTimestamp = Date.now() - expirationMillis
-      const removeExpired = createExpiredDataRemover(get, set, expiredTimestamp)
-      removeExpired(blockResultsAtom)
-      removeExpired(groupResultsAtom)
-      removeExpired(transactionResultsAtom)
-      removeExpired(accountResultsAtom)
-      removeExpired(applicationMetadataResultsAtom)
-      removeExpired(applicationResultsAtom)
-      removeExpired(assetMetadataResultsAtom)
-      removeExpired(assetResultsAtom)
-    })()
+    const expiredTimestamp = Date.now() - expirationMillis
+
+    const removeExpired = createExpiredDataRemover(get, set, expiredTimestamp)
+    set(latestTransactionIdsAtom, (prev) => {
+      const next = prev.filter(([_, timestamp]) => timestamp > expiredTimestamp)
+      if (prev.length !== next.length) {
+        return next
+      }
+      return prev
+    })
+    removeExpired(blockResultsAtom)
+    removeExpired(groupResultsAtom)
+    removeExpired(transactionResultsAtom)
+    removeExpired(accountResultsAtom)
+    removeExpired(applicationMetadataResultsAtom)
+    removeExpired(applicationResultsAtom)
+    removeExpired(assetMetadataResultsAtom)
+    removeExpired(assetResultsAtom)
   }, cleanUpIntervalMillis)
 
   return () => clearInterval(cleanup)
