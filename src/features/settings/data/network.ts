41f8fa9ecc5d916c9470d02732f404042984c31e
Neil Campbell <neil.campbell@makerx.com.au>
2024-06-11 12:26:17 +0800

    chore: change localnet urls

--- src/features/settings/data/network.ts
+++ src/features/settings/data/network.ts
@@ -49,17 +49,17 @@ export const localnetConfig: NetworkConfig = {
   id: 'localnet',
   name: 'LocalNet',
   indexer: {
-    server: 'http://localhost/',
+    server: 'http://localtest.me/',
     port: 8980,
     token: 'aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa',
   },
   algod: {
-    server: 'http://localhost/',
+    server: 'http://localtest.me/',
     port: 4001,
     token: 'aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa',
   },
   kmd: {
-    server: 'http://localhost/',
+    server: 'http://localtest.me/',
     port: 4002,
     token: 'aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa',
   },
