fecd2e1bcac976e3d47e41cdeed7bac71dc1227e
Patrick Dinh <patrick.dinh@makerx.com.au>
2024-06-13 10:00:12 +1000

    improve the graph code

--- src/features/transactions-graph/components/transactions-graph.tsx
+++ src/features/transactions-graph/components/transactions-graph.tsx
@@ -13,57 +13,64 @@ export function TransactionsGraph({ transactionsGraphData }: Props) {
   const maxNestingLevel = Math.max(...horizontalLines.map((h) => h.depth))
   const firstColumnWidth = graphConfig.colWidth + maxNestingLevel * graphConfig.indentationWidth
   const verticalLinesCount = verticalLines.length
-  // An application can have multiple addresses, the headerHeight is the make room for all of them
-  const headerLines = Math.max(...verticalLines.map((s) => (s.type === 'Application' ? s.accounts.length + 1 : 1)))
-  const headerHeight = headerLines > 1 ? headerLines * 0.75 * graphConfig.rowHeight : graphConfig.rowHeight
+  const gridTemplateColumns = `minmax(${firstColumnWidth}px, ${firstColumnWidth}px) repeat(${verticalLinesCount}, ${graphConfig.colWidth}px)`
 
   return (
-    <div
-      className={cn('relative grid')}
-      style={{
-        gridTemplateColumns: `minmax(${firstColumnWidth}px, ${firstColumnWidth}px) repeat(${verticalLinesCount}, ${graphConfig.colWidth}px)`,
-        gridTemplateRows: `${headerHeight}px repeat(${transactionsCount}, ${graphConfig.rowHeight}px)`,
-      }}
-    >
-      <div>{/* The first header cell is empty */}</div>
-      {verticalLines.map((swimlane, index) => (
-        <div className={cn('p-2 flex justify-center')} key={index}>
-          <VerticalLineTitle verticalLine={swimlane} />
-        </div>
-      ))}
-      {/* The below div is for drawing the background dash lines */}
-      <div className={cn('absolute left-0')} style={{ top: `${headerHeight}px` }}>
-        <div>
-          <div className={cn('p-0')}></div>
-          <div
-            className={cn('p-0')}
-            style={{
-              height: `${transactionsCount * graphConfig.rowHeight}px`,
-              width: `${graphConfig.colWidth * verticalLinesCount}px`,
-            }}
-          >
+    <>
+      <div
+        className={cn('relative grid')}
+        style={{
+          gridTemplateColumns: `${gridTemplateColumns}`,
+        }}
+      >
+        <div>{/* The first header cell is empty */}</div>
+        {verticalLines.map((swimlane, index) => (
+          <div className={cn('p-2 flex justify-center')} key={index}>
+            <VerticalLineTitle verticalLine={swimlane} />
+          </div>
+        ))}
+      </div>
+      <div
+        className={cn('relative grid')}
+        style={{
+          gridTemplateColumns: `${gridTemplateColumns}`,
+          gridTemplateRows: `repeat(${transactionsCount}, ${graphConfig.rowHeight}px)`,
+        }}
+      >
+        {/* The below div is for drawing the background dash lines */}
+        <div className={cn('absolute left-0')}>
+          <div>
+            <div className={cn('p-0')}></div>
             <div
-              className={cn('grid h-full')}
+              className={cn('p-0')}
               style={{
-                gridTemplateColumns: `minmax(${firstColumnWidth}px, ${firstColumnWidth}px) repeat(${verticalLinesCount}, ${graphConfig.colWidth}px)`,
                 height: `${transactionsCount * graphConfig.rowHeight}px`,
+                width: `${graphConfig.colWidth * verticalLinesCount}px`,
               }}
             >
-              <div></div>
-              {verticalLines
-                .filter((a) => a.type !== 'Placeholder') // Don't need to draw for the empty swimlane
-                .map((_, index) => (
-                  <div key={index} className={cn('flex justify-center')}>
-                    <div className={cn('border-muted h-full border-dashed')} style={{ borderLeftWidth: graphConfig.lineWidth }}></div>
-                  </div>
-                ))}
+              <div
+                className={cn('grid h-full')}
+                style={{
+                  gridTemplateColumns: `${gridTemplateColumns}`,
+                  height: `${transactionsCount * graphConfig.rowHeight}px`,
+                }}
+              >
+                <div></div>
+                {verticalLines
+                  .filter((a) => a.type !== 'Placeholder') // Don't need to draw for the empty swimlane
+                  .map((_, index) => (
+                    <div key={index} className={cn('flex justify-center')}>
+                      <div className={cn('border-muted h-full border-dashed')} style={{ borderLeftWidth: graphConfig.lineWidth }}></div>
+                    </div>
+                  ))}
+              </div>
             </div>
           </div>
         </div>
+        {horizontalLines.map((row, index) => (
+          <HorizontalLine key={index} verticalLines={verticalLines} horizontalLine={row} />
+        ))}
       </div>
-      {horizontalLines.map((row, index) => (
-        <HorizontalLine key={index} verticalLines={verticalLines} horizontalLine={row} />
-      ))}
-    </div>
+    </>
   )
 }
