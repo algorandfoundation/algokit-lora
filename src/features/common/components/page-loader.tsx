9bfef2d27b802f2c49dda0fdb00547d89e11c4df
Neil Campbell <neil.campbell@makerx.com.au>
2024-06-16 00:54:34 +0800

    chore: add subtle animation on loadable

--- src/features/common/components/page-loader.tsx
+++ src/features/common/components/page-loader.tsx
@@ -2,7 +2,7 @@ import { Loader2 as Loader } from 'lucide-react'
 
 export function PageLoader() {
   return (
-    <div className="m-0 flex flex-col items-center justify-center">
+    <div className="mt-10 flex flex-col items-center justify-center">
       <Loader className="size-16 animate-spin" />
       <span className="mt-2">Loading...</span>
     </div>
