53bb0c35c6c8ee55241ad91489d3bf7677ff5550
Hoang Dinh <patrick.dinh@makerx.com.au>
2024-06-17 13:46:43 +1000

    handle unknown network

--- src/features/network/pages/network-page.tsx
+++ src/features/network/pages/network-page.tsx
@@ -1,21 +1,25 @@
 import { useRequiredParam } from '@/features/common/hooks/use-required-param'
-import { UrlParams, Urls } from '@/routes/urls'
-import { networksConfigs } from '@/features/settings/data'
-import { useNavigate } from 'react-router-dom'
-import { useEffect } from 'react'
+import { UrlParams } from '@/routes/urls'
+import { networksConfigs, useSelectedNetwork } from '@/features/settings/data'
+import { useLocation, useNavigate } from 'react-router-dom'
+import { useEffect, useState } from 'react'
 
 type Props = {
   children: React.ReactNode
 }
 export function NetworkPage({ children }: Props) {
+  const [isValidNetworkId, setIsValidNetworkId] = useState<boolean>(false)
+  const [selectedNetwork] = useSelectedNetwork()
   const { networkId } = useRequiredParam(UrlParams.NetworkId)
   const navigate = useNavigate()
+  const { pathname, search, hash } = useLocation()
 
   useEffect(() => {
-    if (!networksConfigs.find((c) => c.id === networkId)) {
-      navigate(Urls.Index.build({}))
+    if (networkId === '_') {
+      navigate(pathname.replace('_', selectedNetwork) + search + hash)
     }
-  }, [navigate, networkId])
+    setIsValidNetworkId(!!networksConfigs.find((c) => c.id === networkId))
+  }, [hash, pathname, search, navigate, networkId, selectedNetwork])
 
-  return children
+  return isValidNetworkId ? children : <div>Network "{networkId}" is not a valid network.</div>
 }
