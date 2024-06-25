1cb57afe90af8f696ce34dd0325a8f0e98c52ddc
Neil Campbell <neil.campbell@makerx.com.au>
2024-06-25 09:58:14 +0800

    chore: build dappflow url based on selected network (#119)

--- src/features/app-studio/pages/app-studio-page.tsx
+++ src/features/app-studio/pages/app-studio-page.tsx
@@ -1,8 +1,19 @@
 import { PageTitle } from '@/features/common/components/page-title'
+import { useSelectedNetwork } from '@/features/settings/data'
 
 export const appStudioPageTitle = 'App Studio'
 
+const dappflowNetwork = new Map([
+  ['localnet', 'sandbox'],
+  ['testnet', 'algonode_testnet'],
+  ['mainnet', 'algonode_mainnet'],
+])
+
 export function AppStudioPage() {
+  const [selectedNetwork] = useSelectedNetwork()
+  const selectedDappflowNetwork = dappflowNetwork.get(selectedNetwork) ?? dappflowNetwork.get('localnet')!
+  const dappflowUrl = `https://app.dappflow.org/setnetwork?name=${selectedDappflowNetwork}&redirect=beaker-studio/`
+
   return (
     <>
       <PageTitle title={appStudioPageTitle} />
@@ -11,7 +22,7 @@ export function AppStudioPage() {
           Coming soon!
           <br />
           In the meantime, we recommend{' '}
-          <a href="https://app.dappflow.org/beaker-studio/" className="text-primary underline" rel="nofollow" target="_blank">
+          <a href={dappflowUrl} className="text-primary underline" rel="nofollow" target="_blank">
             Dappflow
           </a>{' '}
           for your application needs.
