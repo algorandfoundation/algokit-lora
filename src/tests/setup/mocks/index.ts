8c8996cacf19d2a825be98a804c50b7605923cc1
Patrick Dinh <patrick.dinh@makerx.com.au>
2024-06-28 11:11:37 +1000

    refactor

--- src/tests/setup/mocks/index.ts
+++ src/tests/setup/mocks/index.ts
@@ -1,41 +1,8 @@
-import { Mock, vi } from 'vitest'
+import { vi } from 'vitest'
 import algosdk from 'algosdk'
 import { PROVIDER_ID, useWallet } from '@txnlab/use-wallet'
+import { SearchTransactionsMock } from '@/tests/setup/mocks/search-transactions'
 
-type SearchTransactionsMockArgs = {
-  nextToken?: string
-  limit?: number
-}
-export class SearchTransactionsMock {
-  assetID: Mock
-  applicationID: Mock
-  do: Mock
-  limit: Mock
-  nextToken: Mock
-  txType: Mock
-  address: Mock
-  addressRole: Mock
-  args: SearchTransactionsMockArgs
-
-  constructor() {
-    this.args = {}
-
-    this.assetID = vi.fn().mockReturnThis()
-    this.applicationID = vi.fn().mockReturnThis()
-    this.limit = vi.fn().mockImplementation((args) => {
-      this.args.limit = args
-      return this
-    })
-    this.nextToken = vi.fn().mockImplementation((args) => {
-      this.args.nextToken = args
-      return this
-    })
-    this.txType = vi.fn().mockReturnThis()
-    this.address = vi.fn().mockReturnThis()
-    this.addressRole = vi.fn().mockReturnThis()
-    this.do = vi.fn()
-  }
-}
 export const searchTransactionsMock = new SearchTransactionsMock()
 
 vi.mock('react-router-dom', async () => ({
