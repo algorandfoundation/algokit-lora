d0c6cf87c5f25eef12751e9bf2d0301a60b572e3
Neil Campbell <neil.campbell@makerx.com.au>
2024-07-01 15:50:43 +0800

    chore: remove state dependency

--- src/features/common/hooks/use-scroll-restoration.ts
+++ src/features/common/hooks/use-scroll-restoration.ts
@@ -1,15 +1,12 @@
 import { useEffect } from 'react'
-import { useLocation, useNavigation } from 'react-router-dom'
+import { useLocation } from 'react-router-dom'
 
 // React router scroll restoration doesn't currently work on non body scrollable containers.
 // This is a very simple hook to scroll to the top of the container when the location changes.
 export function useScrollRestoration(container: React.RefObject<HTMLElement>) {
   const location = useLocation()
 
-  const { state } = useNavigation()
   useEffect(() => {
-    if (state === 'idle') {
-      container.current?.scrollTo(0, 0)
-    }
-  }, [location.pathname, state, container])
+    container.current?.scrollTo(0, 0)
+  }, [location.pathname, container])
 }
