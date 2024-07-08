d9161e05ca37d39b037196cf10fe9a4f6edfea29
Neil Campbell <neil.campbell@makerx.com.au>
2024-07-08 15:13:01 +0800

    chore: more tweaks

--- src/features/assets/components/asset-media.tsx
+++ src/features/assets/components/asset-media.tsx
@@ -10,9 +10,9 @@ export function AssetMedia({ asset }: Props) {
   return asset.media ? (
     <Card aria-label={assetMediaLabel}>
       <CardContent className={'flex w-full justify-center md:aspect-square md:w-44'}>
-        {asset.media.type === AssetMediaType.Image && <img className={'size-full object-cover'} src={asset.media.url} alt={asset.name} />}
+        {asset.media.type === AssetMediaType.Image && <img className="size-full object-cover" src={asset.media.url} alt={asset.name} />}
         {asset.media.type === AssetMediaType.Video && (
-          <video title={asset.name} autoPlay playsInline loop controls muted>
+          <video className="size-full object-cover" title={asset.name} autoPlay playsInline loop controls muted>
             <source src={asset.media.url} type="video/mp4" />
           </video>
         )}
