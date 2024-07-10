1714fb3aeeb6cc0db93d99f3a106089659c545e9
Neil Campbell <neil.campbell@makerx.com.au>
2024-07-10 20:49:29 +0800

    fix: refinements (#142)
    * chore: handle overflowing kmd wallets
    * fix: tweak disable opt in/out button tooltip

--- src/features/assets/components/asset-opt-in-out-button.tsx
+++ src/features/assets/components/asset-opt-in-out-button.tsx
@@ -1,24 +1,29 @@
 import { useAssetOptInOut } from '@/features/assets/data/asset-opt-in-out'
 import { RenderLoadable } from '@/features/common/components/render-loadable'
 import { CircleArrowOutDownRight, CircleX, Loader2 as Loader } from 'lucide-react'
-import { Button, AsyncActionButton, AsyncActionButtonProps } from '@/features/common/components/button'
+import { Button, AsyncActionButton } from '@/features/common/components/button'
 import { Asset } from '@/features/assets/models'
 import { Tooltip, TooltipContent, TooltipTrigger } from '@/features/common/components/tooltip'
 
 const optInLabel = 'Opt-in'
 const optOutLabel = 'Opt-out'
 
-function OptInButton({ onClick }: Pick<AsyncActionButtonProps, 'onClick'>) {
+type ButtonProps = {
+  onClick?: () => Promise<void>
+  disabled?: boolean
+}
+
+function OptInButton(props: ButtonProps) {
   return (
-    <AsyncActionButton onClick={onClick} className="w-28" variant="outline-secondary" icon={<CircleArrowOutDownRight size={16} />}>
+    <AsyncActionButton {...props} className="w-28" variant="outline-secondary" icon={<CircleArrowOutDownRight size={16} />}>
       {optInLabel}
     </AsyncActionButton>
   )
 }
 
-function OptOutButton({ onClick }: Pick<AsyncActionButtonProps, 'onClick'>) {
+function OptOutButton(props: ButtonProps) {
   return (
-    <AsyncActionButton onClick={onClick} className="w-28" variant="outline-secondary" icon={<CircleX size={16} />}>
+    <AsyncActionButton {...props} className="w-28" variant="outline-secondary" icon={<CircleX size={16} />}>
       {optOutLabel}
     </AsyncActionButton>
   )
@@ -28,32 +33,36 @@ function LoadingButton() {
   return <Button disabled={true} className="w-28" variant="outline-secondary" icon={<Loader className="size-6 animate-spin" />} />
 }
 
-function DisabledButton() {
-  return (
-    <Tooltip>
-      <TooltipTrigger asChild>
-        <span tabIndex={0}>
-          <Button disabled={true} className="w-28" variant="outline-secondary" icon={<CircleArrowOutDownRight size={16} />}>
-            {optInLabel}
-          </Button>
-        </span>
-      </TooltipTrigger>
-      <TooltipContent>
-        <span>Please connect your wallet to enable this action</span>
-      </TooltipContent>
-    </Tooltip>
-  )
-}
-
 export function AssetOptInOutButton({ asset }: { asset: Asset }) {
   const { status: assetOptInOutStatus, optIn, optOut } = useAssetOptInOut(asset)
 
   return (
     <RenderLoadable loadable={assetOptInOutStatus} fallback={<LoadingButton />}>
       {(status) => {
-        if (!status.canOptIn && !status.canOptOut) return <DisabledButton />
         if (status.canOptIn) return <OptInButton onClick={optIn} />
-        if (status.canOptOut) return <OptOutButton onClick={optOut} />
+        else if (status.canOptOut) return <OptOutButton onClick={optOut} />
+        else {
+          const { reason, button } = !status.hasActiveAccount
+            ? {
+                reason: 'This action requires a connected wallet',
+                button: <OptInButton disabled={true} />,
+              }
+            : {
+                reason: 'This action requires a holding balance of 0',
+                button: <OptOutButton disabled={true} />,
+              }
+
+          return (
+            <Tooltip delayDuration={400}>
+              <TooltipTrigger asChild>
+                <div tabIndex={0}>{button}</div>
+              </TooltipTrigger>
+              <TooltipContent>
+                <span>{reason}</span>
+              </TooltipContent>
+            </Tooltip>
+          )
+        }
       }}
     </RenderLoadable>
   )
