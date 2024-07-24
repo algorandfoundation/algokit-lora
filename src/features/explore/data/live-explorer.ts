e7a37f40915869c79d8c3776f7360e35e5c9e474
Neil Campbell <neil.campbell@makerx.com.au>
2024-07-24 11:54:18 +0800

    fix: don't limit latest transactions to the latest 10 blocks

--- src/features/explore/data/live-explorer.ts
+++ src/features/explore/data/live-explorer.ts
@@ -3,7 +3,7 @@ import { BlockSummary } from '@/features/blocks/models'
 import { TransactionSummary } from '@/features/transactions/models'
 import { atomEffect } from 'jotai-effect'
 import { blockResultsAtom, syncedRoundAtom } from '@/features/blocks/data'
-import { getTransactionResultAtom } from '@/features/transactions/data'
+import { getTransactionResultAtom, latestTransactionIdsAtom } from '@/features/transactions/data'
 import { asTransactionSummary } from '@/features/transactions/mappers'
 import { asBlockSummary } from '@/features/blocks/mappers'
 import { isDefined } from '@/utils/is-defined.ts'
@@ -36,7 +36,8 @@ const liveExplorerAtomsBuilder = () => {
               const block = await get(blockResult[0])
               const transactionSummaries = await Promise.all(
                 block.transactionIds.map(async (transactionId) => {
-                  const transactionResult = await get(getTransactionResultAtom(transactionId, { skipTimestampUpdate: true }))
+                  const transactionResultAtom = getTransactionResultAtom(transactionId, { skipTimestampUpdate: true })
+                  const transactionResult = await get.peek(transactionResultAtom)
                   return asTransactionSummary(transactionResult)
                 })
               )
@@ -47,18 +48,16 @@ const liveExplorerAtomsBuilder = () => {
         )
       ).filter(isDefined)
 
-      const latestTransactionSummaries: TransactionSummary[] = []
-      exit_loops: for (const block of latestBlockSummaries) {
-        const transactions = block.transactions.reverse()
-        for (const transaction of transactions) {
-          latestTransactionSummaries.push(transaction)
-          if (latestTransactionSummaries.length >= maxTransactionsToDisplay) {
-            break exit_loops
-          }
-        }
-      }
-      set(latestTransactionSummariesAtom, latestTransactionSummaries.slice(0, maxTransactionsToDisplay))
+      const latestTransactionIds = get.peek(latestTransactionIdsAtom)
+      const latestTransactionSummaries = await Promise.all(
+        latestTransactionIds.slice(0, maxTransactionsToDisplay).map(async ([transactionId]) => {
+          const transactionResultAtom = getTransactionResultAtom(transactionId, { skipTimestampUpdate: true })
+          const transactionResult = await get.peek(transactionResultAtom)
+          return asTransactionSummary(transactionResult)
+        })
+      )
 
+      set(latestTransactionSummariesAtom, latestTransactionSummaries)
       set(latestBlockSummariesAtom, latestBlockSummaries)
     })()
   })
