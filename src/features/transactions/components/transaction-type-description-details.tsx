37d05c42e9efe23c5badca6c1a4c6d7ce11fdc4b
Neil Campbell <neil.campbell@makerx.com.au>
2024-08-15 16:26:03 +0800

    feat: add delete badge to an app call transaction that is performing a delete

--- src/features/transactions/components/transaction-type-description-details.tsx
+++ src/features/transactions/components/transaction-type-description-details.tsx
@@ -1,4 +1,4 @@
-import { InnerTransaction, SignatureType, Transaction, TransactionType } from '@/features/transactions/models'
+import { AppCallOnComplete, InnerTransaction, SignatureType, Transaction, TransactionType } from '@/features/transactions/models'
 import { Badge } from '@/features/common/components/badge'
 
 export function TransactionTypeDescriptionDetails({ transaction }: { transaction: Transaction | InnerTransaction }) {
@@ -7,6 +7,9 @@ export function TransactionTypeDescriptionDetails({ transaction }: { transaction
       <Badge variant={transaction.type}>{transaction.type}</Badge>
       {transaction.subType && <Badge variant="outline">{transaction.subType}</Badge>}
       {transaction.type === TransactionType.AppCall && transaction.isOpUp && <Badge variant="outline">OpUp</Badge>}
+      {transaction.type === TransactionType.AppCall && transaction.onCompletion === AppCallOnComplete.Delete && (
+        <Badge variant="outline">Delete</Badge>
+      )}
       {transaction.signature?.type === SignatureType.Multi && <Badge variant="outline">Multisig</Badge>}
       {transaction.signature?.type === SignatureType.Logic && <Badge variant="outline">LogicSig</Badge>}
       {transaction.rekeyTo && <Badge variant="outline">Rekey</Badge>}
