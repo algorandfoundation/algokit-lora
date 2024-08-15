d6b6503b34e995a7ba2885ba1a5afc6d815e9eae
Neil Campbell <neil.campbell@makerx.com.au>
2024-08-15 13:41:51 +0800

    chore: show how to add download as a button

--- src/features/transactions-graph/components/transactions-graph.tsx
+++ src/features/transactions-graph/components/transactions-graph.tsx
@@ -4,7 +4,8 @@ import { graphConfig } from '@/features/transactions-graph/components/graph-conf
 import { VerticalTitle } from '@/features/transactions-graph/components/vertical-title'
 import { Horizontal } from '@/features/transactions-graph/components/horizontal'
 import html2canvas from 'html2canvas-pro'
-import { useEffect } from 'react'
+import { useCallback } from 'react'
+import { Button } from '@/features/common/components/button'
 
 type Props = {
   transactionsGraphData: TransactionsGraphData
@@ -17,76 +18,108 @@ export function TransactionsGraph({ transactionsGraphData }: Props) {
   const verticalsCount = verticals.length
   const gridTemplateColumns = `minmax(${horizontalTitleWidth}px, ${horizontalTitleWidth}px) repeat(${verticalsCount}, ${graphConfig.colWidth}px)`
 
-  useEffect(() => {
-    html2canvas(document.getElementById('visual-transactions')!, {
+  // useEffect(() => {
+  //   html2canvas(document.getElementById('visual-transactions')!, {
+  //     foreignObjectRendering: false,
+  //     removeContainer: true,
+  //     backgroundColor: '#001223',
+  //   }).then((canvas) => {
+  //     const dataUrl = canvas.toDataURL()
+
+  //     // console.log(dataUrl)
+  //     // window.location.href = dataUrl
+
+  //     const link = document.createElement('a')
+  //     link.text = 'Download image'
+  //     link.href = dataUrl
+  //     link.setAttribute('download', 'transactions-graph.png')
+  //     document.body.appendChild(link)
+  //     link.click()
+
+  //     // const img = new Image()
+  //     // img.src = dataUrl
+  //     // document.body.appendChild(img)
+  //   })
+  // }, [])
+
+  const downloadImage = useCallback(async () => {
+    const visual = document.getElementById('visual-transactions')!
+    const canvas = await html2canvas(visual, {
       foreignObjectRendering: false,
       removeContainer: true,
       backgroundColor: '#001223',
-    }).then((canvas) => {
-      const dataUrl = canvas.toDataURL()
-      const img = new Image()
-      img.src = dataUrl
-      document.body.appendChild(img)
     })
+    const dataUrl = canvas.toDataURL()
+    visual.setAttribute('data-url', dataUrl)
+    const link = document.createElement('a')
+    link.text = 'Download image'
+    // link.className = 'hidden'
+    link.href = dataUrl
+    link.setAttribute('download', 'transactions-graph.png')
+    // document.body.appendChild(link)
+    link.click()
   }, [])
 
   return (
-    // Don't change this id value, it's used by a bot Alessandro is building.
-    <div id="visual-transactions">
-      <div
-        className={cn('relative grid')}
-        style={{
-          gridTemplateColumns: `${gridTemplateColumns}`,
-        }}
-      >
-        <div>{/* The first header cell is empty */}</div>
-        {verticals.map((vertical, index) => (
-          <div className={cn('p-2 flex justify-center')} key={index}>
-            <VerticalTitle vertical={vertical} />
-          </div>
-        ))}
-      </div>
-      <div
-        className={cn('relative grid')}
-        style={{
-          gridTemplateColumns: `${gridTemplateColumns}`,
-          gridTemplateRows: `repeat(${horizontalsCount}, ${graphConfig.rowHeight}px)`,
-        }}
-      >
-        {/* The below div is for drawing the background dash lines */}
-        <div className={cn('absolute left-0')}>
-          <div>
-            <div className={cn('p-0')}></div>
-            <div
-              className={cn('p-0')}
-              style={{
-                height: `${horizontalsCount * graphConfig.rowHeight}px`,
-                width: `${graphConfig.colWidth * verticalsCount}px`,
-              }}
-            >
+    <>
+      <Button onClick={downloadImage}>Download image</Button>
+      {/* Don't change this id value, it's used by a bot Alessandro is building. */}
+      <div id="visual-transactions">
+        <div
+          className={cn('relative grid')}
+          style={{
+            gridTemplateColumns: `${gridTemplateColumns}`,
+          }}
+        >
+          <div>{/* The first header cell is empty */}</div>
+          {verticals.map((vertical, index) => (
+            <div className={cn('p-2 flex justify-center')} key={index}>
+              <VerticalTitle vertical={vertical} />
+            </div>
+          ))}
+        </div>
+        <div
+          className={cn('relative grid')}
+          style={{
+            gridTemplateColumns: `${gridTemplateColumns}`,
+            gridTemplateRows: `repeat(${horizontalsCount}, ${graphConfig.rowHeight}px)`,
+          }}
+        >
+          {/* The below div is for drawing the background dash lines */}
+          <div className={cn('absolute left-0')}>
+            <div>
+              <div className={cn('p-0')}></div>
               <div
-                className={cn('grid h-full')}
+                className={cn('p-0')}
                 style={{
-                  gridTemplateColumns: `${gridTemplateColumns}`,
                   height: `${horizontalsCount * graphConfig.rowHeight}px`,
+                  width: `${graphConfig.colWidth * verticalsCount}px`,
                 }}
               >
-                <div></div>
-                {verticals
-                  .filter((a) => a.type !== 'Placeholder') // Don't need to draw for the empty vertical
-                  .map((_, index) => (
-                    <div key={index} className={cn('flex justify-center')}>
-                      <div className={cn('border h-full border-dashed')} style={{ borderLeftWidth: graphConfig.lineWidth }}></div>
-                    </div>
-                  ))}
+                <div
+                  className={cn('grid h-full')}
+                  style={{
+                    gridTemplateColumns: `${gridTemplateColumns}`,
+                    height: `${horizontalsCount * graphConfig.rowHeight}px`,
+                  }}
+                >
+                  <div></div>
+                  {verticals
+                    .filter((a) => a.type !== 'Placeholder') // Don't need to draw for the empty vertical
+                    .map((_, index) => (
+                      <div key={index} className={cn('flex justify-center')}>
+                        <div className={cn('border h-full border-dashed')} style={{ borderLeftWidth: graphConfig.lineWidth }}></div>
+                      </div>
+                    ))}
+                </div>
               </div>
             </div>
           </div>
+          {horizontals.map((row, index) => (
+            <Horizontal key={index} verticals={verticals} horizontal={row} />
+          ))}
         </div>
-        {horizontals.map((row, index) => (
-          <Horizontal key={index} verticals={verticals} horizontal={row} />
-        ))}
       </div>
-    </div>
+    </>
   )
 }
