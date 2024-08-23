ad171aa10972b7524897e40b9e705598e842abe3
Negar Abbasi <negar.abbasi@makerx.com.au>
2024-08-23 11:54:58 +1000

    chore: fix the comment

--- src/features/transactions-graph/components/transactions-graph.tsx
+++ src/features/transactions-graph/components/transactions-graph.tsx
@@ -96,7 +96,7 @@ export function TransactionsGraph({ transactionsGraphData }: Props) {
         </div>
       </div>
       <div className="sticky bottom-0 left-full z-50 flex size-0 overflow-visible">
-        // Don't change this id value, it's used by a bot Alessandro is building.
+        {/* Don't change this id value, it's used by a bot Alessandro is building. */}
         <Button id="download-transactions-visual" className="absolute bottom-1 right-0 w-32" variant="outline" onClick={downloadImage}>
           <Download className="mr-2 size-4" />
           Download
