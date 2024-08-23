c3c90f21518fd36259942aa75ad34fd79f858fb4
Hoang Dinh <d.conghoang@gmail.com>
2024-08-23 19:54:30 +1000

    WIP - can run v2 without any plugin

--- src/features/deep-link/hooks/use-deep-link.tsx
+++ src/features/deep-link/hooks/use-deep-link.tsx
@@ -4,45 +4,50 @@ import { useNavigate } from 'react-router-dom'
 import { useSetSelectedNetwork } from '@/features/network/data'
 import { parseDeepLink } from '@/features/deep-link/parse-deep-link'
 import { Urls } from '@/routes/urls'
+// import { onOpenUrl } from '@tauri-apps/plugin-deep-link'
+
+// await onOpenUrl((urls) => {
+//   console.log('deep link:', urls)
+// })
 
 export function useDeepLink() {
-  const setSelectedNetwork = useSetSelectedNetwork()
-  const navigate = useNavigate()
-
-  const handleDeepLink = useCallback(
-    async (url: string | undefined) => {
-      const options = parseDeepLink(url)
-      if (options) {
-        await setSelectedNetwork(options.networkId)
-        if (options.transactionId) {
-          navigate(Urls.Explore.Transaction.ById.build({ transactionId: options.transactionId, networkId: options.networkId }))
-        } else {
-          navigate(Urls.Index.build({}))
-        }
-      }
-    },
-    [navigate, setSelectedNetwork]
-  )
-
-  useEffect(() => {
-    if (!window.__TAURI__) {
-      return
-    }
-
-    if (window.deepLink) {
-      // On init
-      handleDeepLink(window.deepLink).then(() => {
-        // Reset so that it won't be used again
-        window.deepLink = undefined
-      })
-    }
-    // On deep link event while the app is open
-    const unlisten = listen('deep-link-received', (event) => {
-      handleDeepLink(event.payload as string)
-    })
-
-    return () => {
-      unlisten.then((f) => f())
-    }
-  }, [handleDeepLink])
+  // const setSelectedNetwork = useSetSelectedNetwork()
+  // const navigate = useNavigate()
+  //
+  // const handleDeepLink = useCallback(
+  //   async (url: string | undefined) => {
+  //     const options = parseDeepLink(url)
+  //     if (options) {
+  //       await setSelectedNetwork(options.networkId)
+  //       if (options.transactionId) {
+  //         navigate(Urls.Explore.Transaction.ById.build({ transactionId: options.transactionId, networkId: options.networkId }))
+  //       } else {
+  //         navigate(Urls.Index.build({}))
+  //       }
+  //     }
+  //   },
+  //   [navigate, setSelectedNetwork]
+  // )
+  //
+  // useEffect(() => {
+  //   if (!window.__TAURI__) {
+  //     return
+  //   }
+  //
+  //   if (window.deepLink) {
+  //     // On init
+  //     handleDeepLink(window.deepLink).then(() => {
+  //       // Reset so that it won't be used again
+  //       window.deepLink = undefined
+  //     })
+  //   }
+  //   // On deep link event while the app is open
+  //   const unlisten = listen('deep-link-received', (event) => {
+  //     handleDeepLink(event.payload as string)
+  //   })
+  //
+  //   return () => {
+  //     unlisten.then((f) => f())
+  //   }
+  // }, [handleDeepLink])
 }
