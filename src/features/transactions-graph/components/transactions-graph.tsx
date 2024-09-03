deb7f7de01f334eda37c11776c74812e12be3bba
Neil Campbell <neil.campbell@makerx.com.au>
2024-09-03 13:15:56 +0800

    chore: finalise

--- src/features/transactions-graph/components/transactions-graph.tsx
+++ src/features/transactions-graph/components/transactions-graph.tsx
@@ -50,7 +50,7 @@ export function TransactionsGraph({ transactionsGraphData, downloadable, bgClass
     link.setAttribute('download', transactionsGraphData.filename)
     // TODO: This approach won't work in Tauri, so we'll need to handle with Tauri's APIs
     link.click()
-  }, [])
+  }, [transactionsGraphData.filename])
 
   const bgClassName = _bgClassName ?? 'bg-card'
 
