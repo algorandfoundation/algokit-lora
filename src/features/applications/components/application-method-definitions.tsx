5a5151c6437890ac561a6e3a2253d587580c52ad
Neil Campbell <neil.campbell@makerx.com.au>
2024-09-05 14:48:07 +0800

    chore: render method call result

--- src/features/applications/components/application-method-definitions.tsx
+++ src/features/applications/components/application-method-definitions.tsx
@@ -1,7 +1,7 @@
 import { ApplicationAbiMethods, ArgumentDefinition, MethodDefinition, ReturnsDefinition } from '@/features/applications/models'
 import { Accordion, AccordionContent, AccordionItem, AccordionTrigger } from '@/features/common/components/accordion'
 import { DescriptionList } from '@/features/common/components/description-list'
-import { useCallback, useMemo } from 'react'
+import { useCallback, useMemo, useState } from 'react'
 import { Struct as StructType, DefaultArgument as DefaultArgumentType } from '@/features/app-interfaces/data/types/arc-32/application'
 import { Form } from '@/features/forms/components/form'
 import { FormActions } from '@/features/forms/components/form-actions'
@@ -17,11 +17,29 @@ import { invariant } from '@/utils/invariant'
 import { AppSpec } from '@algorandfoundation/algokit-utils/types/app-spec'
 import { ABIAppCallArg } from '@algorandfoundation/algokit-utils/types/app'
 import { extractArgumentIndexFromFieldPath } from '../mappers'
+import { toast } from 'react-toastify'
+import { TransactionsGraph, TransactionsGraphData } from '@/features/transactions-graph'
+import { asTransactionsGraphData } from '@/features/transactions-graph/mappers'
+import { asTransaction } from '@/features/transactions/mappers'
+import { getIndexerTransactionFromAlgodTransaction } from '@algorandfoundation/algokit-subscriber/transform'
+import { assetSummaryResolver } from '@/features/assets/data'
+import { abiMethodResolver } from '@/features/abi-methods/data'
+import { transactionIdLabel } from '@/features/transactions/components/transaction-info'
+import { TransactionLink } from '@/features/transactions/components/transaction-link'
+import { DecodedAbiMethodReturnValue } from '@/features/abi-methods/components/decoded-abi-method-return-value'
+import { BlockInnerTransaction, BlockTransaction } from '@algorandfoundation/algokit-subscriber/types/block'
+import { TransactionType } from '@/features/transactions/models'
+import { Atom } from 'jotai'
+import { AbiMethod } from '@/features/abi-methods/models'
+import { RenderInlineAsyncAtom } from '@/features/common/components/render-inline-async-atom'
+import algosdk from 'algosdk'
 
 type Props<TSchema extends z.ZodSchema> = {
   applicationId: ApplicationId
   abiMethods: ApplicationAbiMethods<TSchema> // TODO: NC - Get the naming right
 }
+
+const connectWalletMessage = 'Please connect a wallet'
 
 // TODO: NC - ABI Methods?
 export function ApplicationMethodDefinitions<TSchema extends z.ZodSchema>({ applicationId, abiMethods }: Props<TSchema>) {
@@ -45,14 +63,21 @@ type MethodProps<TSchema extends z.ZodSchema> = {
   appSpec?: Arc32AppSpec // TODO: NC - We don't really need to support ARC4 here, so think about this, we need to support arc56 though
 }
 
+type SendMethodCallResult = {
+  transactionId: string
+  abiMethod: Atom<Promise<AbiMethod | undefined>>
+  transactionsGraphData: TransactionsGraphData
+}
+
 function Method<TSchema extends z.ZodSchema>({ applicationId, method, appSpec }: MethodProps<TSchema>) {
   const { activeAddress, signer } = useWallet()
-
+  const [sendMethodCallResult, setSendMethodCallResult] = useState<SendMethodCallResult | undefined>(undefined)
   type TData = z.infer<typeof method.schema>
 
   const sendMethodCall = useCallback(
     async (data: TData) => {
       invariant(appSpec, 'An ARC-32 app spec is required when calling ABI methods')
+      invariant(activeAddress, connectWalletMessage)
 
       const methodArgs = Object.entries(data).reduce((acc, [path, value]) => {
         const index = extractArgumentIndexFromFieldPath(path)
@@ -65,21 +90,57 @@ function Method<TSchema extends z.ZodSchema>({ applicationId, method, appSpec }:
         app: appSpec as AppSpec,
       })
 
-      // TODO: NC - Increase valid rounds, to allow time for signing
       // TODO: NC - Move to the new AlgorandClient approach when ready
-      await client.call({
+      const result = await client.call({
         method: method.name,
         methodArgs,
         sender: {
-          addr: activeAddress!, // TODO: NC - Handle this properly
+          addr: activeAddress,
           signer,
         },
       })
 
-      // TODO: NC - Show success message
-      // TODO: NC - Render the transaction ID + graph
+      const transactionId = result.transaction.txID()
+      const confirmation = result.confirmation!
 
-      // TODO: NC - Do we need to allow opt-in to the app? Only needed when the app uses local state. Probably a new story.
+      const mapBlockTransaction = (res: algosdk.modelsv2.PendingTransactionResponse): BlockTransaction | BlockInnerTransaction => {
+        return {
+          txn: res.txn.txn,
+          dt: {
+            // We don't use gd or ld in this context, so don't need to map.
+            gd: {},
+            ld: {},
+            lg: res.logs ?? [],
+            itx: res.innerTxns?.map((inner) => mapBlockTransaction(inner)),
+          },
+        }
+      }
+
+      // TODO: NC - Propagate any changes here into the other places
+      const transactionResult = getIndexerTransactionFromAlgodTransaction({
+        blockTransaction: mapBlockTransaction(confirmation),
+        roundOffset: 0,
+        roundIndex: 0,
+        genesisHash: confirmation.txn.txn.gh,
+        genesisId: confirmation.txn.txn.gen,
+        roundNumber: Number(confirmation.confirmedRound),
+        roundTimestamp: Math.floor(Date.now() / 1000),
+        transaction: result.transactions[0],
+        logs: confirmation.logs,
+      })
+
+      const transaction = asTransaction(transactionResult, assetSummaryResolver, abiMethodResolver)
+      invariant(transaction.type === TransactionType.AppCall, 'AppCall transaction expected')
+
+      const transactionsGraphData = asTransactionsGraphData([transaction])
+
+      setSendMethodCallResult({
+        transactionId,
+        abiMethod: transaction.abiMethod,
+        transactionsGraphData,
+      })
+
+      toast.success('Transaction sent successfully')
     },
     [activeAddress, appSpec, applicationId, method.arguments, method.name, signer]
   )
@@ -126,6 +187,31 @@ function Method<TSchema extends z.ZodSchema>({ applicationId, method, appSpec }:
             </>
           )}
         </Form>
+        {sendMethodCallResult && (
+          <div className="my-4 flex flex-col gap-4 text-sm">
+            <DescriptionList
+              items={[
+                {
+                  dt: transactionIdLabel,
+                  dd: (
+                    <TransactionLink transactionId={sendMethodCallResult.transactionId} className="text-sm text-primary underline">
+                      {sendMethodCallResult.transactionId}
+                    </TransactionLink>
+                  ),
+                },
+                {
+                  dt: 'Return value',
+                  dd: (
+                    <RenderInlineAsyncAtom atom={sendMethodCallResult.abiMethod}>
+                      {(abiMethod) => (abiMethod ? <DecodedAbiMethodReturnValue methodReturn={abiMethod?.return} /> : 'void')}
+                    </RenderInlineAsyncAtom>
+                  ),
+                },
+              ]}
+            />
+            <TransactionsGraph transactionsGraphData={sendMethodCallResult.transactionsGraphData} downloadable={false} />
+          </div>
+        )}
       </AccordionContent>
     </AccordionItem>
   )
