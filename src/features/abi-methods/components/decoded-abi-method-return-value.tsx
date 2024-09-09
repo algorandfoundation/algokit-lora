3b3666923934a4e632fc66b60d93d815e411f9ca
Negar Abbasi <negar.abbasi@makerx.com.au>
2024-09-09 13:22:01 +1000

    chore: some renamings

--- src/features/abi-methods/components/decoded-abi-method-return-value.tsx
+++ src/features/abi-methods/components/decoded-abi-method-return-value.tsx
@@ -1,10 +1,10 @@
 import { AbiValue } from '@/features/abi-methods/components/abi-value'
 import { AbiMethod } from '@/features/abi-methods/models'
-import { asAbiValueRender } from '@/features/abi-methods/mappers'
+import { asAbiValueRepresentation } from '@/features/abi-methods/mappers'
 
 export function DecodedAbiMethodReturnValue({ method }: { method: AbiMethod }) {
   const methodReturn = method.return
   if (methodReturn === 'void') return 'void'
-  const methodReturnRender = asAbiValueRender(methodReturn)
-  return <AbiValue abiValue={methodReturnRender} />
+  const methodReturnRepresentation = asAbiValueRepresentation(methodReturn)
+  return <AbiValue abiValue={methodReturnRepresentation} />
 }
--- src/features/abi-methods/mappers/index.ts
+++ src/features/abi-methods/mappers/index.ts
@@ -17,6 +17,8 @@ import {
 } from '@/features/abi-methods/models'
 import { sum } from '@/utils/sum'
 
+const MAX_LENGTH = 20
+
 export const jsonAsArc32AppSpec = (json: unknown): Arc32AppSpec => {
   const validator = new Validator()
   validator.addSchema(Arc4ContractSchema, '/arc4-contract.schema.json')
@@ -35,7 +37,7 @@ export const jsonAsArc4AppSpec = (json: unknown): Arc4AppSpec => {
   return json as Arc4AppSpec
 }
 
-export const asAbiMethodArgumentRender = (abiMethodArgument: AbiMethodArgument): AbiMethodArgumentRepresentation => {
+export const asAbiMethodArgumentRepresentation = (abiMethodArgument: AbiMethodArgument): AbiMethodArgumentRepresentation => {
   if (
     abiMethodArgument.type === AbiType.Address ||
     abiMethodArgument.type === AbiType.Application ||
@@ -49,34 +51,35 @@ export const asAbiMethodArgumentRender = (abiMethodArgument: AbiMethodArgument):
       length: `${abiMethodArgument.value}`.length,
     }
   }
-  const renderedArgument = asAbiValueRender(abiMethodArgument)
-  const multiLine = sum([renderedArgument.length]) > 20
+  const resolvedArgument = asAbiValueRepresentation(abiMethodArgument)
+  const multiLine = sum([resolvedArgument.length]) > MAX_LENGTH
 
   return {
     ...abiMethodArgument,
-    ...renderedArgument,
+    ...resolvedArgument,
     multiLine: multiLine,
   }
 }
 
 export function getAbiMethodRepresentation(method: AbiMethod): AbiMethodRepresentation {
-  const argumentsRepresentation = method.arguments.map((argument) => asAbiMethodArgumentRender(argument))
+  const argumentRepresentationList = method.arguments.map((argument) => asAbiMethodArgumentRepresentation(argument))
   const multiLine =
-    argumentsRepresentation.some((argument) => argument.multiLine) || sum(argumentsRepresentation.map((arg) => arg.length)) > 20
+    argumentRepresentationList.some((argument) => argument.multiLine) ||
+    sum(argumentRepresentationList.map((arg) => arg.length)) > MAX_LENGTH
   return {
     name: method.name,
-    arguments: argumentsRepresentation,
+    arguments: argumentRepresentationList,
     multiLine: multiLine,
     return: method.return,
   }
 }
 
-export const asAbiValueRender = (abiValue: AbiValue): AbiValueRepresentation => {
+export const asAbiValueRepresentation = (abiValue: AbiValue): AbiValueRepresentation => {
   if (abiValue.type === AbiType.Tuple) {
-    return asAbiTupleRender(abiValue)
+    return asAbiTupleRepresentation(abiValue)
   }
   if (abiValue.type === AbiType.Array) {
-    return asAbiArrayRender(abiValue)
+    return asAbiArrayRepresentation(abiValue)
   }
   return {
     ...abiValue,
@@ -85,25 +88,25 @@ export const asAbiValueRender = (abiValue: AbiValue): AbiValueRepresentation => 
   }
 }
 
-const asAbiTupleRender = (abiTuple: AbiTupleValue): AbiTupleRepresentation => {
-  const valuesRender = abiTuple.values.map((value) => asAbiValueRender(value))
-  const length = sum(valuesRender.map((r) => r.length))
-  const multiLine = valuesRender.some((value) => value.multiLine) || length > 90
+const asAbiTupleRepresentation = (abiTuple: AbiTupleValue): AbiTupleRepresentation => {
+  const valueRepresentation = abiTuple.values.map((value) => asAbiValueRepresentation(value))
+  const length = sum(valueRepresentation.map((r) => r.length))
+  const multiLine = valueRepresentation.some((value) => value.multiLine) || length > MAX_LENGTH
   return {
     type: AbiType.Tuple,
-    values: valuesRender,
+    values: valueRepresentation,
     multiLine: multiLine,
     length: length,
   }
 }
 
-const asAbiArrayRender = (abiArray: AbiArrayValue): AbiArrayRepresentation => {
-  const valuesRender = abiArray.values.map((value) => asAbiValueRender(value))
-  const length = sum(valuesRender.map((r) => r.length))
-  const multiLine = valuesRender.some((value) => value.multiLine) || length > 90
+const asAbiArrayRepresentation = (abiArray: AbiArrayValue): AbiArrayRepresentation => {
+  const valueRepresentation = abiArray.values.map((value) => asAbiValueRepresentation(value))
+  const length = sum(valueRepresentation.map((r) => r.length))
+  const multiLine = valueRepresentation.some((value) => value.multiLine) || length > MAX_LENGTH
   return {
     type: AbiType.Array,
-    values: valuesRender,
+    values: valueRepresentation,
     multiLine: multiLine,
     length: length,
   }
