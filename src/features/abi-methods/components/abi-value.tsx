d61cb6bb937810357d89b9066ec069965233e66d
Negar Abbasi <negar.abbasi@makerx.com.au>
2024-09-11 13:53:07 +1000

    chore: little tweak

--- src/features/abi-methods/components/abi-value.tsx
+++ src/features/abi-methods/components/abi-value.tsx
@@ -24,6 +24,6 @@ export function AbiValue({ abiValue }: Props) {
     return <span className="text-abi-bool">{abiValue.value}</span>
   }
   if (abiValue.type === AbiType.Address) {
-    return <AccountLink className="text-primary underline" address={abiValue.value}></AccountLink>
+    return <AccountLink address={abiValue.value}></AccountLink>
   }
 }
