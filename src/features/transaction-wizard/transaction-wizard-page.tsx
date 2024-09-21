2fba313e68fd715d4108cc9c9da1c3755227eb30
Hoang Dinh <patrick.dinh@makerx.com.au>
2024-09-22 05:49:10 +1000

    show result transaction graph

--- src/features/transaction-wizard/transaction-wizard-page.tsx
+++ src/features/transaction-wizard/transaction-wizard-page.tsx
@@ -6,14 +6,26 @@ import { TransactionBuilder } from './components/transaction-builder'
 import { algorandClient } from '../common/data/algo-client'
 import { useWallet } from '@txnlab/use-wallet'
 import { invariant } from '@/utils/invariant'
+import { TransactionsGraph, TransactionsGraphData } from '../transactions-graph'
+import { DescriptionList } from '../common/components/description-list'
+import { transactionIdLabel } from '../transactions/components/transaction-info'
+import { TransactionLink } from '../transactions/components/transaction-link'
+import { asTransactionsGraphData } from '../transactions-graph/mappers'
+import { asTransactionFromSendResult } from '../transactions/data/send-transaction-result'
 
 export const transactionWizardPageTitle = 'Transaction Wizard'
 export const transactionTypeLabel = 'Transaction type'
 export const sendButtonLabel = 'Send'
 
+type SendTransactionResult = {
+  transactionId: string
+  transactionsGraphData: TransactionsGraphData
+}
+
 export function TransactionWizardPage() {
   const { activeAddress, signer } = useWallet()
   const [transactions, setTransactions] = useState<algosdk.Transaction[]>([])
+  const [sendTransactionResult, setSendTransactionResult] = useState<SendTransactionResult | undefined>(undefined)
 
   const { open, dialog } = useDialogForm({
     dialogHeader: 'Transaction Builder',
@@ -31,7 +43,6 @@ export function TransactionWizardPage() {
 
   const sendTransactions = useCallback(async () => {
     invariant(activeAddress, 'Please connect your wallet')
-    console.log(transactions)
 
     const atc = algorandClient.setSigner(activeAddress, signer).newGroup()
     transactions.forEach((transaction) => {
@@ -39,8 +50,15 @@ export function TransactionWizardPage() {
     })
 
     const result = await atc.execute()
-    console.log(result)
-  }, [transactions])
+    const sentTxns = asTransactionFromSendResult(result)
+    const transactionId = result.txIds[0]
+    const transactionsGraphData = asTransactionsGraphData(sentTxns)
+
+    setSendTransactionResult({
+      transactionId,
+      transactionsGraphData,
+    })
+  }, [activeAddress, signer, transactions])
 
   return (
     <div>
@@ -52,6 +70,27 @@ export function TransactionWizardPage() {
       <Button onClick={openDialog}>Create</Button>
       <Button onClick={sendTransactions}>Send</Button>
       {dialog}
+      {sendTransactionResult && (
+        <div className="my-4 flex flex-col gap-4 text-sm">
+          <DescriptionList
+            items={[
+              {
+                dt: transactionIdLabel,
+                dd: (
+                  <TransactionLink transactionId={sendTransactionResult.transactionId} className="text-sm text-primary underline">
+                    {sendTransactionResult.transactionId}
+                  </TransactionLink>
+                ),
+              },
+            ]}
+          />
+          <TransactionsGraph
+            transactionsGraphData={sendTransactionResult.transactionsGraphData}
+            bgClassName="bg-background"
+            downloadable={false}
+          />
+        </div>
+      )}
     </div>
   )
 }
