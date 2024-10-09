3c56ffd99727fac06cb7308fb82b42292018020d
Negar Abbasi <negar.abbasi@makerx.com.au>
2024-10-09 15:52:23 +1000

    fix: add stale

--- src/features/nfd/data/nfd.ts
+++ src/features/nfd/data/nfd.ts
@@ -1,18 +1,28 @@
 import { Address } from '@/features/accounts/data/types'
-import { useAtomValue } from 'jotai'
+import { atom, useAtomValue, useSetAtom } from 'jotai'
 import { getNfdResultAtom } from './nfd-result'
 import { atomWithRefresh, loadable } from 'jotai/utils'
 import { useMemo } from 'react'
+import { atomEffect } from 'jotai-effect'
 
 const createNfdAtoms = (address: Address) => {
+  const isStaleAtom = atom(false)
+  const detectIsStaleEffect = atomEffect((get, set) => {
+    const nfdResults = get(getNfdResultAtom({ address: address }))
+    const isStale = nfdResults === undefined
+    set(isStaleAtom, isStale)
+  })
+
   return [
     atomWithRefresh(async (get) => {
       const nfdResult = await get(getNfdResultAtom({ address: address }))
+      get(detectIsStaleEffect)
       if (!nfdResult) {
         return null
       }
       return nfdResult
     }),
+    isStaleAtom,
   ] as const
 }
 
@@ -23,6 +33,6 @@ export const useNfdAtoms = (address: Address) => {
 }
 
 export const useLoadableNfd = (address: Address) => {
-  const [nfdAtom] = useNfdAtoms(address)
-  return [useAtomValue(loadable(nfdAtom))] as const
+  const [nfdAtom, isStaleAtom] = useNfdAtoms(address)
+  return [useAtomValue(loadable(nfdAtom)), useSetAtom(nfdAtom), useAtomValue(isStaleAtom)] as const
 }
