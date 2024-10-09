4351b4627e0acba0ccc0e76039088efdd260f2cb
Neil Campbell <neil.campbell@makerx.com.au>
2024-10-09 12:58:34 +0800

    chore: utf-8 decode box info where applicable (#272)

--- src/features/applications/components/application-box-details-dialog.tsx
+++ src/features/applications/components/application-box-details-dialog.tsx
@@ -7,15 +7,20 @@ import { ApplicationId } from '../data/types'
 import { RenderLoadable } from '@/features/common/components/render-loadable'
 import { useLoadableApplicationBox } from '../data/application-boxes'
 import { Dialog, DialogContent, DialogHeader, DialogTrigger, MediumSizeDialogBody } from '@/features/common/components/dialog'
+import { base64ToUtf8IfValid } from '@/utils/base64-to-utf8'
 
 type Props = { applicationId: ApplicationId; boxName: string }
 
 const dialogTitle = 'Application Box'
 export function ApplicationBoxDetailsDialog({ applicationId, boxName }: Props) {
+  const decodedBoxName = useMemo(() => {
+    return base64ToUtf8IfValid(boxName)
+  }, [boxName])
+
   return (
     <Dialog modal={true}>
       <DialogTrigger>
-        <label className={cn('text-primary underline cursor-pointer')}>{boxName}</label>
+        <label className={cn('text-primary underline cursor-pointer')}>{decodedBoxName}</label>
       </DialogTrigger>
       <DialogContent>
         <DialogHeader>
@@ -40,23 +45,25 @@ function InternalDialogContent({ applicationId, boxName }: Props) {
 }
 
 function ApplicationBoxDetails({ applicationBox }: { applicationBox: ApplicationBox }) {
-  const items = useMemo(
-    () => [
+  const items = useMemo(() => {
+    const decodedBoxName = base64ToUtf8IfValid(applicationBox.name)
+    const decodedBoxValue = base64ToUtf8IfValid(applicationBox.value)
+
+    return [
       {
         dt: applicationBoxNameLabel,
-        dd: applicationBox.name,
+        dd: decodedBoxName,
       },
       {
         dt: applicationBoxValueLabel,
         dd: (
           <div className="grid">
-            <div className="overflow-y-auto break-words"> {applicationBox.value}</div>
+            <div className="overflow-y-auto break-words"> {decodedBoxValue}</div>
           </div>
         ),
       },
-    ],
-    [applicationBox.name, applicationBox.value]
-  )
+    ]
+  }, [applicationBox.name, applicationBox.value])
 
   return <DescriptionList items={items} />
 }
