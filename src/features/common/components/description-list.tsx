da06eaa8b26b61de4aa331d06bdbc9d908866fce
Negar Abbasi <negar.abbasi@makerx.com.au>
2024-10-30 14:45:01 +1000

    fix: ensures the dtClassName classes will take precedence.

--- src/features/common/components/description-list.tsx
+++ src/features/common/components/description-list.tsx
@@ -15,7 +15,7 @@ export function DescriptionList({ items, dtClassName }: Props) {
     <div className={cn('grid grid-cols-[minmax(min-content,auto)_1fr] gap-x-4 gap-y-1.5')}>
       {items.map((item, index) => (
         <dl key={index} className={cn('grid grid-cols-subgrid col-span-2')}>
-          <dt className={cn(dtClassName, 'font-medium')}>{item.dt}</dt>
+          <dt className={cn('font-medium', dtClassName)}>{item.dt}</dt>
           <dd className={cn('overflow-ellipsis whitespace-normal overflow-hidden')}>
             {typeof item.dd === 'bigint' ? item.dd.toString() : item.dd}
           </dd>
