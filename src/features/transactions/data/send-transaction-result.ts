e86587ee9ca2b8b328b5f67d535fe6b2ac5a711a
Hoang Dinh <patrick.dinh@makerx.com.au>
2024-12-11 09:49:28 +1000

    fix send-transaction-result

--- src/features/transactions/data/send-transaction-result.ts
+++ src/features/transactions/data/send-transaction-result.ts
@@ -1,8 +1,5 @@
-import { BlockInnerTransaction } from '@algorandfoundation/algokit-subscriber/types/block'
-import algosdk from 'algosdk'
 import { Transaction } from '../models'
 import { SendTransactionResults } from '@algorandfoundation/algokit-utils/types/transaction'
-import { getIndexerTransactionFromAlgodTransaction } from '@algorandfoundation/algokit-subscriber/transform'
 import { invariant } from '@/utils/invariant'
 import { asTransaction } from '../mappers'
 import { getAssetResultAtom } from '@/features/assets/data'
@@ -12,24 +9,7 @@ import { GroupId, GroupResult } from '@/features/groups/data/types'
 import { Round } from '@/features/blocks/data/types'
 import { AssetId } from '@/features/assets/data/types'
 import { asAssetSummary } from '@/features/assets/mappers'
-
-const asBlockTransaction = (res: algosdk.modelsv2.PendingTransactionResponse): BlockInnerTransaction => {
-  return {
-    txn: res.txn.txn,
-    sgnr: res.txn.sgnr?.publicKey,
-    caid: res.assetIndex,
-    apid: res.applicationIndex,
-    aca: res.assetClosingAmount,
-    ca: res.closingAmount,
-    dt: {
-      // We don't use gd or ld in this context, so don't need to map.
-      gd: {},
-      ld: {},
-      lg: res.logs ?? [],
-      itx: res.innerTxns?.map((inner) => asBlockTransaction(inner)),
-    },
-  }
-}
+import { TransactionResult } from './types'
 
 export const asTransactionFromSendResult = (result: SendTransactionResults): Transaction[] => {
   if (!result.confirmations || result.confirmations.length === 0 || result.transactions.length === 0) {
@@ -49,25 +29,15 @@ export const asTransactionFromSendResult = (result: SendTransactionResults): Tra
     })
 
   // This mapping does some approximations, which are fine for the contexts we currently use it for.
-  return result.confirmations.map((confirmation, i) => {
+  return result.confirmations.map((confirmation) => {
     invariant(confirmation.txn.txn.genesisHash, 'Genesis hash is required')
     invariant(confirmation.txn.txn.genesisID, 'Genesis ID is required')
 
-    const txnResult = getIndexerTransactionFromAlgodTransaction({
-      blockTransaction: asBlockTransaction(confirmation),
-      roundOffset: 0,
-      roundIndex: 0,
-      genesisHash: Buffer.from(confirmation.txn.txn.genesisHash),
-      genesisId: confirmation.txn.txn.genesisID,
-      roundNumber: Number(confirmation.confirmedRound ?? 0),
-      roundTimestamp: Math.floor(now.getTime() / 1000),
-      transaction: result.transactions[i],
-      logs: confirmation.logs,
-      createdAssetId: confirmation.assetIndex,
-      createdAppId: confirmation.applicationIndex,
-      closeAmount: confirmation.closingAmount,
-      assetCloseAmount: confirmation.assetClosingAmount,
-    })
+    // TODO: PD - test
+    const txnResult = {
+      ...confirmation.txn.txn,
+      sender: confirmation.txn.txn.sender.toString(),
+    } satisfies TransactionResult
 
     const transaction = asTransaction(txnResult, assetSummaryResolver, abiMethodResolver, groupResolver)
     return transaction
