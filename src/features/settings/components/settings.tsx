5ead859841483608cc803777877373bfc9dcadbe
Hoang Dinh <patrick.dinh@makerx.com.au>
2025-01-10 16:27:19 +1000

    only build appimage + show version

--- src/features/settings/components/settings.tsx
+++ src/features/settings/components/settings.tsx
@@ -1,11 +1,19 @@
+import { useGetVersion } from '@/features/tauri/use-get-version'
 import { ClearCache } from './clear-cache'
 import { NetworkConfigsTable } from '@/features/network/components/network-configs-table'
 
 export function Settings() {
+  const version = useGetVersion()
+
   return (
     <div className="flex flex-col space-y-8">
       <NetworkConfigsTable />
       <ClearCache />
+      {version && (
+        <div className="flex flex-col items-end">
+          <p>Version: {version}</p>
+        </div>
+      )}
     </div>
   )
 }
