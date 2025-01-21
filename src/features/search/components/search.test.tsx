b5b0793ede5c694942334ff4b4099787d7578ff1
David Rojas <lempira@gmail.com>
2025-01-21 17:35:56 -0500

    feat: Added nav links to search results

--- src/features/search/components/search.test.tsx
+++ src/features/search/components/search.test.tsx
@@ -89,6 +89,11 @@ describe('search', () => {
             const input = component.getByPlaceholderText(searchPlaceholderLabel)
             await user.type(input, id)
             const results = (await component.findAllByText(label, undefined, { timeout: 1000 })).map((result) => result.parentElement)
+            results.forEach((result) => {
+              const link = result!.querySelector('a')
+              const linkText = link?.textContent || ''
+              expect(linkText.substring(0, 3)).toBe(id.substring(0, 3))
+            })
             const result = results.find((result) => result!.textContent!.includes(type))!
             await user.click(result)
 
