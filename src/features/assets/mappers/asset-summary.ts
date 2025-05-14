eef7a707a8c843d0430e4c9eec49f672f8bcab06
Neil Campbell <neil.campbell@makerx.com.au>
2025-05-15 00:06:04 +0800

    fix: correctly handle b64 asset names which are utf8 strings

--- src/features/assets/mappers/asset-summary.ts
+++ src/features/assets/mappers/asset-summary.ts
@@ -1,10 +1,13 @@
 import { AssetResult } from '../data/types'
 import { AssetSummary } from '../models'
+import { bytesToBase64 } from 'algosdk'
+import { base64ToUtf8IfValid } from '@/utils/base64-to-utf8'
 
 export const asAssetSummary = (assetResult: AssetResult): AssetSummary => {
   return {
     id: assetResult.index,
-    name: assetResult.params.name ?? assetResult.params.nameB64?.toString(),
+    name:
+      assetResult.params.name ?? (assetResult.params.nameB64 ? base64ToUtf8IfValid(bytesToBase64(assetResult.params.nameB64)) : undefined),
     decimals: Number(assetResult.params.decimals),
     unitName: assetResult.params.unitName,
     creator: assetResult.params.creator,
