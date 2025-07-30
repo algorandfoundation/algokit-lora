c89d5b6b5315986d6a657db6224738fb1b4834b0
p2 <arthurrabelopro@gmail.com>
2025-07-29 22:46:54 -0700

    [sync] Align local branch with latest changes from main

--- src/features/transactions/mappers/arc-2.ts
+++ src/features/transactions/mappers/arc-2.ts
@@ -3,6 +3,11 @@ import { Arc2TransactionNote } from '@algorandfoundation/algokit-utils/types/tra
 // Based on the ARC-2 spec https://github.com/algorandfoundation/ARCs/blob/main/ARCs/arc-0002.md#specification
 const arc2Regex = /^([a-zA-Z0-9][a-zA-Z0-9_/@.-]{4,31}):([mjbu]{1})(.*)$/
 
+// Created this function to check if a string is an ARC-2 transaction note to follow the spec
+export function isArc2(maybeArc2: string): boolean {
+  return arc2Regex.test(maybeArc2)
+}
+
 export function parseArc2(maybeArc2: string) {
   const result = maybeArc2.match(arc2Regex)
   if (result && result.length === 4) {
