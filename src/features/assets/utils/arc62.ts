ddf49abe7b072442e113b26cab8adfec60f57298
p2 <arthurrabelopro@gmail.com>
2025-07-29 22:42:02 -0700

    [upd:isArc62] Update isArc62() to check for 'application-id' inside 'arc-62' metadata property

--- src/features/assets/utils/arc62.ts
+++ src/features/assets/utils/arc62.ts
@@ -1,8 +1,12 @@
 import { Arc3MetadataResult } from '../data/types'
 
 // Checks if the asset metadata has the ARC-62 property in the correct place
-export const hasArc62Property = (asset: Arc3MetadataResult) => {
-  return asset.metadata.properties?.['arc-62'] !== undefined
+export const isArc62 = (asset: Arc3MetadataResult): boolean => {
+  if (!asset?.metadata?.properties) return false
+
+  const arc62 = asset.metadata.properties['arc-62'] as { 'application-id'?: number }
+
+  return arc62?.['application-id'] !== undefined
 }
 
 // Plaeholder function to get the circulating supply from the application ID
