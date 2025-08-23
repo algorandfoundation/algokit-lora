6e3fc47405e86acc1b1d4bcd4aecd52951610c29
p2arthur <arthurrabelopro@gmail.com>
2025-08-22 20:05:29 -0700

    feat (network-aware_funded_discovery) implement network awareness to the funded discovery method so it automatically uses localnet funded wallet to query the blockchain

--- src/utils/funded-discovery.ts
+++ src/utils/funded-discovery.ts
@@ -1,22 +1,37 @@
 import { AppClientMethodCallParamsArgs } from '@/features/applications/data/types'
 import { algorandClient } from '@/features/common/data/algo-client'
-import { FEE_SINK_ADDRESS } from '@/features/network/data'
-import { ABIMethod } from 'algosdk'
+import { FEE_SINK_ADDRESS, networkConfigAtom } from '@/features/network/data'
+import { settingsStore } from '@/features/settings/data'
+import { ABIMethod, Address } from 'algosdk'
+import { AlgorandClient } from '@algorandfoundation/algokit-utils'
 
 export async function executeFundedDiscoveryApplicationCall(
   applicationMethod: ABIMethod,
   applicationId: bigint,
   applicationCallArgs?: AppClientMethodCallParamsArgs[]
 ) {
+  let fundedDiscoveryAddress: string | Address
+
+  const networkConfig = settingsStore.get(networkConfigAtom)
+
+  if (!networkConfig.id) return
+
+  fundedDiscoveryAddress = FEE_SINK_ADDRESS
+
+  if (networkConfig.id === 'localnet') {
+    const localnetClient = AlgorandClient.defaultLocalNet()
+
+    fundedDiscoveryAddress = await localnetClient.account.fromEnvironment('FUNDED_DISCOVERY_ACCOUNT', (1).algo())
+  }
+
   // Create a transaction composer to call the method
   const composer = algorandClient.newGroup()
-
   // Add the method call to the composer
   composer.addAppCallMethodCall({
     appId: applicationId,
     method: applicationMethod,
     args: applicationCallArgs,
-    sender: FEE_SINK_ADDRESS,
+    sender: fundedDiscoveryAddress,
   })
   const simulateResult = await composer.simulate({ skipSignatures: true, allowUnnamedResources: true })
 
