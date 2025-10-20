54709eee5279856eae9b2486ea73660b0c6130a4
p2arthur <arthurrabelopro@gmail.com>
2025-10-20 12:12:30 -0700

    revert(revert_experiment) revert experiment to make table scrollable pushed to further pr

--- src/features/transaction-wizard/components/transactions-builder.tsx
+++ src/features/transaction-wizard/components/transactions-builder.tsx
@@ -339,8 +339,8 @@ export function TransactionsBuilder({
   }, [activeAddress, commonButtonDisableProps])
 
   return (
-    <div className="w-screen">
-      <div className="space-y-4 w-full overflow-scroll">
+    <div>
+      <div className="space-y-4">
         <div className="mb-4 flex items-center gap-2">
           {title}
           {!disableAddTransaction && (
