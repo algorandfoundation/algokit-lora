08f3b5c4d067ec8da315e437e1a0db524026c06a
p2arthur <arthurrabelopro@gmail.com>
2025-10-23 00:51:52 -0700

    fix(cursor_pointer) fixed cursor pointer on buttons as global class

--- src/features/common/components/button.tsx
+++ src/features/common/components/button.tsx
@@ -7,7 +7,7 @@ import { Loader2 as Loader } from 'lucide-react'
 import { Tooltip, TooltipContent, TooltipTrigger } from './tooltip'
 
 const buttonVariants = cva(
-  'relative cursor-pointer inline-flex items-center justify-center whitespace-nowrap rounded-md text-sm font-medium ring-offset-background transition-colors focus-visible:outline-hidden focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50',
+  'relative inline-flex items-center justify-center whitespace-nowrap rounded-md text-sm font-medium ring-offset-background transition-colors focus-visible:outline-hidden focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50',
   {
     variants: {
       variant: {
