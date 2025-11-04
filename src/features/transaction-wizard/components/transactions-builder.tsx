41773a27975bd375d8fa7591a7e147949468e322
p2arthur <arthurrabelopro@gmail.com>
2025-11-03 22:02:17 -0800

    fix(wizard_visual) fix wizard actions overflowing + fix visual txn button overlapping nodes

--- src/features/transaction-wizard/components/transactions-builder.tsx
+++ src/features/transaction-wizard/components/transactions-builder.tsx
@@ -381,7 +381,7 @@ export function TransactionsBuilder({
           </div>
         )}
 
-        <div className="flex items-center justify-between gap-2">
+        <div className="flex flex-wrap items-center justify-between gap-2">
           <div className="flex gap-2">
             {additionalActions}
             {!disablePopulate && (
